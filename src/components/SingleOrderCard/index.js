3efe44230d792bfb02b234d705ea537eb03ddb50
carlos quiroz <carloseqv29596@gmail.com>
2020-08-21 13:28:07 -0400

    [FIX] console errro

--- src/components/SingleOrderCard/index.js
+++ src/components/SingleOrderCard/index.js
@@ -19,11 +19,6 @@ export const SingleOrderCard = (props) => {
 
 SingleOrderCard.propTypes = {
   /**
-   * Instace of Ordering Class
-   * @see See (Ordering API SDK)[https://github.com/sergioaok/ordering-api-sdk]
-   */
-  ordering: PropTypes.object.isRequired,
-  /**
    * UI Component, this must be containt all graphic elements and use parent props
    */
   UIComponent: PropTypes.elementType,
