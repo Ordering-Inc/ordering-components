ab78846073c2b1bd8f4e2e42cf17eb1242d52545
carlos quiroz <carloseqv29596@gmail.com>
2020-08-26 16:22:46 -0400

    [ADD] business products categories component

--- example/views/Home/index.js
+++ example/views/Home/index.js
@@ -59,6 +59,9 @@ export const Home = () => {
         <li>
           <Link to='/product_option_example'>Product option example</Link>
         </li>
+        <li>
+          <Link to='/business_products_categories'>Business products categories</Link>
+        </li>
       </ul>
     </>
   )
