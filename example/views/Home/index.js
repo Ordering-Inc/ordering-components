36e10f75f456889e21661d593a556c1e364611a7
carlos quiroz <carloseqv29596@gmail.com>
2020-09-01 15:41:10 -0400

    [UPDATE] change to orden list

--- example/views/Home/index.js
+++ example/views/Home/index.js
@@ -139,14 +139,14 @@ export const Home = () => {
         </li>
       </ol>
       <h2>Payments Section</h2>
-      <ul>
+      <ol>
         <li>
           <Link to='/payment_option_paypal'>Payments Options Paypal</Link>
         </li>
         <li>
           <Link to='/payment_option_cash'>Payments Options Cash</Link>
         </li>
-      </ul>
+      </ol>
     </>
   )
 }
