52e8b3c6ae2ef0bff1bacb287030ada6dae2d5e5
Jesus Perez <jesusdpch00@gmail.com>
2020-09-04 14:55:15 -0500

    [ADD] basic handling of the user data

--- example/components/AppleLoginUI/index.js
+++ example/components/AppleLoginUI/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import AppleLogin from 'react-apple-login'
 
 export const AppleLoginUI = () => {
   return (
     <>
-    {<h2>AppleLogin</h2>}
-      <AppleLogin clientId="com.react.apple.login" redirectURI="https://redirectUrl.com" />
+      {<h2>AppleLogin</h2>}
+      <div id='appleid-signin' data-color='black' data-border='true' data-type='sign in' />
     </>
   )
 }
--- src/components/AppleLogin/index.js
+++ src/components/AppleLogin/index.js
@@ -1,11 +1,41 @@
-import React from 'react'
-import { Ordering } from 'ordering-api-sdk'
+import React, { useEffect } from 'react'
 
 export const AppleLogin = (props) => {
-  const {Ordering,UIComponent} = props
+  const { UIComponent } = props
+
+  useEffect(() => {
+    console.log(window.AppleID)
+    window.AppleID.auth.init({
+      clientId: 'co.ordering.logintest',
+      redirectURI: 'https://example-app.com/redirect',
+      scope: 'email',
+      usePopup: true
+    })
+
+    fetchData()
+  })
+
+  const fetchData = async () => {
+    try {
+      const data = await window.AppleID.auth.signIn()
+      console.log(data)
+    } catch (err) {
+      console.log('error', err)
+    }
+  }
+
+  /* const createScript = () => {
+    const js = window.document.createElement('script')
+    js.src = "https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"
+    js.async = true
+    js.defer = true
+    window.document.body.appendChild(js)
+    console.log(window.AppleID)
+  } */
+
   return (
     <>
-    {UIComponent && <UIComponent />}
+      {UIComponent && <UIComponent {...props} />}
     </>
   )
 }
