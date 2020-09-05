45cccb94dbe35d255a5ccfe686c3ee28cead8b04
Jesus Perez <jesusdpch00@gmail.com>
2020-09-05 09:36:01 -0500

    add script of apple-login

--- src/components/AppleLogin/index.js
+++ src/components/AppleLogin/index.js
@@ -1,41 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
 export const AppleLogin = (props) => {
-  const { UIComponent } = props
+  const { UIComponent } = props;
 
   useEffect(() => {
-    console.log(window.AppleID)
-    window.AppleID.auth.init({
-      clientId: 'co.ordering.logintest',
-      redirectURI: 'https://example-app.com/redirect',
-      scope: 'email',
-      usePopup: true
-    })
+    createScript()
+  },[])
 
-    fetchData()
-  })
+  const x = () => {
+    window.AppleID.auth.init({
+      clientId: "co.ordering.logintest",
+      redirectURI: "https://example-app.com/redirect",
+      responseType: "code",
+      responseMode: "query",
+      usePopup: true,
+    });
+    //Listen for authorization success
+    document.addEventListener("AppleIDSignInOnSuccess", (data) => {
+      console.log("data", data);
+    });
+    //Listen for authorization failures
+    document.addEventListener("AppleIDSignInOnFailure", (error) => {
+      console.log("errorList", error);
+    });
+    fetchData();
+    console.log(window.AppleID);
+  };
 
   const fetchData = async () => {
     try {
-      const data = await window.AppleID.auth.signIn()
-      console.log(data)
+      const data = await window.AppleID.auth.signIn();
+      console.log(data);
     } catch (err) {
-      console.log('error', err)
+      console.log("error", err);
     }
-  }
+  };
 
-  /* const createScript = () => {
-    const js = window.document.createElement('script')
-    js.src = "https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"
-    js.async = true
-    js.defer = true
-    window.document.body.appendChild(js)
-    console.log(window.AppleID)
-  } */
+  const createScript = () => {
+    const js = window.document.createElement("script");
+    js.id = "apple-login";
+    js.src =
+      "https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js";
+    js.async = true;
+    js.defer = true;
+    js.onload = () => {
+      x()
+    }; 
+    window.document.body.appendChild(js);
+    console.log(window.AppleID);
+  };
 
-  return (
-    <>
-      {UIComponent && <UIComponent {...props} />}
-    </>
-  )
-}
+  return <>{UIComponent && <UIComponent {...props} />}</>;
+};
