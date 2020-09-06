ad148a744ffc453d5c12a778c41f07f667b673d4
Jesus Perez <jesusdpch00@gmail.com>
2020-09-05 20:31:22 -0400

    formating code

--- example/views/AppleLoginExample/index.js
+++ example/views/AppleLoginExample/index.js
@@ -1,14 +1,41 @@
 import React from 'react'
 
-import {AppleLoginUI} from "../../components/AppleLoginUI"
-import {AppleLogin} from "../../../src/components/AppleLogin"
+import { AppleLoginUI } from '../../components/AppleLoginUI'
+import { AppleLogin } from '../../../src/components/AppleLogin'
+import { TestComponent } from '../../components/TestComponent'
 
-export const AppleLoginExample = ({ordering}) => {
+export const AppleLoginExample = ({ ordering }) => {
   const props = {
+    /**
+     * Instace of Ordering Class
+     * @see See (Ordering API SDK)[https://github.com/sergioaok/ordering-api-sdk]
+     */
     ordering: ordering,
-
-    UIComponent: AppleLoginUI
+    /**
+     * UI Component, this must be containt all graphic elements and use parent props
+     */
+    UIComponent: AppleLoginUI,
+    /**
+     * Components types before business information
+     * Array of type components, the parent props will pass to these components
+     */
+    beforeComponents: [TestComponent],
+    /**
+     * Components types after business information
+     * Array of type components, the parent props will pass to these components
+     */
+    afterComponents: [TestComponent],
+    /**
+     * Elements before business information
+     * Array of HTML/Components elements, these components will not get the parent props
+     */
+    beforeElements: [<p key>Test Element Before</p>],
+    /**
+     * Elements after business information
+     * Array of HTML/Components elements, these components will not get the parent props
+     */
+    afterElements: [<p key>Test Element After</p>]
   }
 
-  return <AppleLogin {...props}/>
+  return <AppleLogin {...props} />
 }
--- src/components/AppleLogin/index.js
+++ src/components/AppleLogin/index.js
@@ -1,54 +1,108 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
 
 export const AppleLogin = (props) => {
-  const { UIComponent } = props;
+  const { UIComponent } = props
 
   useEffect(() => {
-    createScript()
-  },[])
+    createScriptApple()
+  }, [])
 
-  const x = () => {
-    window.AppleID.auth.init({
-      clientId: "co.ordering.logintest",
-      redirectURI: "https://example-app.com/redirect",
-      responseType: "code",
-      responseMode: "query",
-      usePopup: true,
-    });
-    //Listen for authorization success
-    document.addEventListener("AppleIDSignInOnSuccess", (data) => {
-      console.log("data", data);
-    });
-    //Listen for authorization failures
-    document.addEventListener("AppleIDSignInOnFailure", (error) => {
-      console.log("errorList", error);
-    });
-    fetchData();
-    console.log(window.AppleID);
-  };
-
-  const fetchData = async () => {
-    try {
-      const data = await window.AppleID.auth.signIn();
-      console.log(data);
-    } catch (err) {
-      console.log("error", err);
-    }
-  };
-
-  const createScript = () => {
-    const js = window.document.createElement("script");
-    js.id = "apple-login";
+  /**
+   * loading script of de Apple login sdk
+   */
+  const createScriptApple = () => {
+    const js = window.document.createElement('script')
+    js.id = 'apple-login'
     js.src =
-      "https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js";
-    js.async = true;
-    js.defer = true;
+      'https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js'
+    js.async = true
+    js.defer = true
     js.onload = () => {
-      x()
-    }; 
-    window.document.body.appendChild(js);
-    console.log(window.AppleID);
-  };
+      initLoginApple()
+    }
+    window.document.body.appendChild(js)
+    console.log(window.AppleID)
+  }
 
-  return <>{UIComponent && <UIComponent {...props} />}</>;
-};
+  /**
+   * start Login Apple
+   */
+  const initLoginApple = () => {
+    window.AppleID.auth.init({
+      clientId: 'co.ordering.logintest',
+      redirectURI: 'https://example-app.com/redirect',
+      responseType: 'code',
+      responseMode: 'query',
+      usePopup: true
+    })
+
+    handleLoginApple()
+    console.log(window.AppleID)
+  }
+
+  /**
+   * handilng the response of login apple when login
+   */
+  const handleLoginApple = async () => {
+    try {
+      const data = await window.AppleID.auth.signIn()
+      console.log(data)
+    } catch (err) {
+      console.log('error', err)
+    }
+  }
+
+  return <>{UIComponent && <UIComponent {...props} />}</>
+}
+
+AppleLogin.propTypes = {
+  /**
+   * Instace of Ordering Class
+   * @see See (Ordering API SDK)[https://github.com/sergioaok/ordering-api-sdk]
+   */
+  ordering: PropTypes.object.isRequired,
+  /**
+   * UI Component, this must be containt all graphic elements and use parent props
+   */
+  UIComponent: PropTypes.elementType,
+  /**
+   * loading script of de Apple login sdk
+   */
+  createScriptApple: PropTypes.func,
+  /**
+   * start Login Apple
+   */
+  initLoginApple: PropTypes.func,
+  /**
+   * handilng the response of login apple when login
+   */
+  handleLoginApple: PropTypes.func,
+  /**
+   * Components types before Facebook login button
+   * Array of type components, the parent props will pass to these components
+   */
+  beforeComponents: PropTypes.arrayOf(PropTypes.elementType),
+  /**
+   * Components types after Facebook login button
+   * Array of type components, the parent props will pass to these components
+   */
+  afterComponents: PropTypes.arrayOf(PropTypes.elementType),
+  /**
+   * Elements before Facebook login button
+   * Array of HTML/Components elements, these components will not get the parent props
+   */
+  beforeElements: PropTypes.arrayOf(PropTypes.element),
+  /**
+   * Elements after Facebook login button
+   * Array of HTML/Components elements, these components will not get the parent props
+   */
+  afterElements: PropTypes.arrayOf(PropTypes.element)
+}
+
+AppleLogin.defaultProps = {
+  beforeComponents: [],
+  afterComponents: [],
+  beforeElements: [],
+  afterElements: []
+}
