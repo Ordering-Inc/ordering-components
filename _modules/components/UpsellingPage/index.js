9b7c9ba97be9ff0f18b0f812afcb1f6c1ebe2bc1
carlos quiroz <carloseqv29596@gmail.com>
2020-10-08 16:57:58 -0400

    build upselling page

---
+++ _modules/components/UpsellingPage/index.js
@@ -0,0 +1,238 @@
+"use strict";
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.UpsellingPage = void 0;
+
+var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _OrderContext = require("../../contexts/OrderContext");
+
+var _ApiContext = require("../../../src/contexts/ApiContext");
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var UpsellingPage = function UpsellingPage(props) {
+  var UIComponent = props.UIComponent,
+      onSave = props.onSave,
+      businessId = props.businessId,
+      products = props.products;
+
+  var _useOrder = (0, _OrderContext.useOrder)(),
+      _useOrder2 = _slicedToArray(_useOrder, 2),
+      orderState = _useOrder2[0],
+      addProduct = _useOrder2[1].addProduct;
+
+  var _useState = (0, _react.useState)({
+    products: [],
+    loading: false,
+    error: false
+  }),
+      _useState2 = _slicedToArray(_useState, 2),
+      productsList = _useState2[0],
+      setProductsList = _useState2[1];
+
+  var _useApi = (0, _ApiContext.useApi)(),
+      _useApi2 = _slicedToArray(_useApi, 1),
+      ordering = _useApi2[0];
+
+  var _useState3 = (0, _react.useState)([]),
+      _useState4 = _slicedToArray(_useState3, 2),
+      upsellingProducts = _useState4[0],
+      setUpsellingProducts = _useState4[1];
+
+  (0, _react.useEffect)(function () {
+    if (products.length || businessId) {
+      if (products.length) {
+        handleProductsOfCart(products);
+      } else {
+        getProducts();
+      }
+    } else {
+      setProductsList(_objectSpread(_objectSpread({}, productsList), {}, {
+        error: true,
+        message: 'BusinessId is required when products is not defined'
+      }));
+    }
+  }, []);
+  /**
+   * getting products if array of product is not defined
+   */
+
+  var getProducts = /*#__PURE__*/function () {
+    var _ref = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee() {
+      var _yield$ordering$busin, result;
+
+      return _regenerator.default.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              _context.prev = 0;
+              setProductsList(_objectSpread(_objectSpread({}, productsList), {}, {
+                loading: true
+              }));
+              _context.next = 4;
+              return ordering.businesses(businessId).products().parameters({
+                type: 1
+              }).get();
+
+            case 4:
+              _yield$ordering$busin = _context.sent;
+              result = _yield$ordering$busin.content.result;
+              setProductsList(_objectSpread(_objectSpread({}, productsList), {}, {
+                loading: false,
+                products: result
+              }));
+              handleProductsOfCart(result);
+              _context.next = 13;
+              break;
+
+            case 10:
+              _context.prev = 10;
+              _context.t0 = _context["catch"](0);
+              setProductsList(_objectSpread(_objectSpread({}, productsList), {}, {
+                loading: false,
+                error: _context.t0
+              }));
+
+            case 13:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee, null, [[0, 10]]);
+    }));
+
+    return function getProducts() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+  /**
+   *
+   * filt products if they are already in the cart
+   * @param {array} cartProducts
+   */
+
+
+  var getUpsellingProducts = function getUpsellingProducts(cartProducts, result) {
+    setUpsellingProducts(result.filter(function (product) {
+      return product.upselling && cartProducts.map(function (cartProduct) {
+        return product.id !== cartProduct.id;
+      });
+    }));
+  };
+  /**
+   * products of the cart
+   */
+
+
+  var handleProductsOfCart = function handleProductsOfCart(result) {
+    var cartProducts = Object.values(orderState.carts).map(function (cart) {
+      return cart === null || cart === void 0 ? void 0 : cart.products.map(function (product) {
+        return product;
+      });
+    });
+    getUpsellingProducts(cartProducts, result);
+  };
+  /**
+   * adding product to the cart from upselling
+   * @param {object} product Product object
+   */
+
+
+  var handleAddProductUpselling = /*#__PURE__*/function () {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee2(product) {
+      var successful;
+      return _regenerator.default.wrap(function _callee2$(_context2) {
+        while (1) {
+          switch (_context2.prev = _context2.next) {
+            case 0:
+              _context2.next = 2;
+              return addProduct(product);
+
+            case 2:
+              successful = _context2.sent;
+              console.log(orderState);
+              console.log(successful);
+
+              if (successful) {
+                onSave(product);
+              }
+
+            case 6:
+            case "end":
+              return _context2.stop();
+          }
+        }
+      }, _callee2);
+    }));
+
+    return function handleAddProductUpselling(_x) {
+      return _ref2.apply(this, arguments);
+    };
+  }();
+
+  return /*#__PURE__*/_react.default.createElement(UIComponent, _extends({}, props, {
+    handleAddProductUpselling: handleAddProductUpselling,
+    upsellingProducts: upsellingProducts,
+    productsList: productsList
+  }));
+};
+
+exports.UpsellingPage = UpsellingPage;
+UpsellingPage.propTypes = {
+  /**
+   * UI Component, this must be containt all graphic elements and use parent props
+   */
+  UIComponent: _propTypes.default.elementType,
+
+  /**
+    * upselling products that do not repeat in the cart
+   */
+  upsellingProducts: _propTypes.default.array,
+
+  /**
+   * Function to save event
+   */
+  onSave: _propTypes.default.func,
+
+  /**
+   * BusinessId is required when products is not defined
+   */
+  businessId: _propTypes.default.number
+};
