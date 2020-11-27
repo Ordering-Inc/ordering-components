a497e5cad37391fe4f93e8c92b0d5d9919257686
carlos quiroz <carloseqv29596@gmail.com>
2020-11-27 11:05:36 -0400

    [FEAT] add validation in google maps

--- src/components/GoogleMaps/index.js
+++ src/components/GoogleMaps/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { WrapperGoogleMaps } from '../WrapperGoogleMaps'
 
@@ -6,17 +6,28 @@ export const GoogleMaps = (props) => {
   const {
     googleReady,
     location,
+    locations,
     mapControls,
     handleChangePosition
   } = props
 
   const divRef = useRef()
+  const [googleMap, setGoogleMap] = useState(null)
+
+  const setMarkers = (map) => {
+    for (let i = 0; i < locations.length; i++) {
+      const marker = new window.google.maps.Marker({
+        position: new window.google.maps.LatLng(locations[i], locations[i]),
+        map
+      })
+    }
+  }
 
   useEffect(() => {
     if (googleReady) {
       const coordinates = { lat: location.lat, lng: location.lng }
       const map = new window.google.maps.Map(divRef.current, {
-        zoom: location.zoom,
+        zoom: location.zoom || mapControls.defaultZoom,
         center: coordinates,
         zoomControl: mapControls?.zoomControl,
         streetViewControl: mapControls?.streetViewControl,
@@ -29,17 +40,33 @@ export const GoogleMaps = (props) => {
           ...mapControls?.mapTypeControlOptions
         }
       })
-      const marker = new window.google.maps.Marker({
-        position: coordinates,
-        map,
-        draggable: true,
-        title: ''
-      })
-      marker.addListener('mouseup', marker => {
-        handleChangePosition(marker.latLng)
-      })
+
+      setGoogleMap(map)
+
+      let marker = null
+      if (locations) {
+        setMarkers(map)
+      } else {
+        marker = new window.google.maps.Marker({
+          position: new window.google.maps.LatLng(coordinates.lat, coordinates.lng),
+          map,
+          draggable: true
+        })
+        marker.addListener('mouseup', marker => {
+          handleChangePosition(marker.latLng)
+        })
+      }
     }
   }, [googleReady])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (googleReady) {
+        setMarkers(googleMap)
+      }
+    }, 2000)
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     googleReady && <div style={{ width: '70%', height: '50%', position: 'absolute' }} id='map' ref={divRef} />
