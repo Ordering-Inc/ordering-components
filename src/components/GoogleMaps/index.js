675bd4cf6026c02fa53c311a40b47c02bb3350d4
carlos quiroz☕ <carloseqv29596@gmail.com>
2020-12-08 08:18:00 -0400

    Google maps improvements (#157)
    * [FEAT] move map with pin and add limit to set marker position
    
    * [FEAT] add variable to manage max limit set location
    
    * [FEAT] validation when there isn't max limite position
    
    * [FIX]
    
    * [FEAT] improve methods to save address forms
    
    * [FEAT] add some validations
    
    * [FIX] error when user hasn't auth

--- src/components/GoogleMaps/index.js
+++ src/components/GoogleMaps/index.js
@@ -19,7 +19,9 @@ export const GoogleMaps = (props) => {
     location,
     locations,
     mapControls,
-    handleChangePosition
+    setErrors,
+    handleChangeAddressMap,
+    maxLimitLocation
   } = props
 
   const divRef = useRef()
@@ -27,6 +29,12 @@ export const GoogleMaps = (props) => {
   const [markers, setMarkers] = useState([])
   const [boundMap, setBoundMap] = useState(null)
 
+  const center = { lat: location.lat, lng: location.lng }
+
+  /**
+   * Function to generate multiple markers
+   * @param {Google map} map
+   */
   const generateMarkers = (map) => {
     const bounds = new window.google.maps.LatLngBounds()
     for (let i = 0; i < locations.length; i++) {
@@ -44,12 +52,70 @@ export const GoogleMaps = (props) => {
     setBoundMap(bounds)
   }
 
+  /**
+   * function to get all address information with a location
+   * @param {google location} pos
+   */
+  const geocodePosition = (pos, map, marker) => {
+    const geocoder = new window.google.maps.Geocoder()
+
+    geocoder.geocode({ latLng: pos }, (results) => {
+      let zipcode = null
+      if (results && results.length > 0) {
+        for (const component of results[0].address_components) {
+          const addressType = component.types[0]
+          if (addressType === 'postal_code') {
+            zipcode = component.short_name
+            break
+          }
+        }
+        const address = {
+          address: results[0].formatted_address,
+          location: { lat: pos.lat(), lng: pos.lng() },
+          zipcode
+        }
+        handleChangeAddressMap(address)
+
+        center.lat = address.location.lat
+        center.lng = address.location.lng
+      } else {
+        marker && marker.setPosition(center)
+        setErrors && setErrors('ERROR_NOT_FOUND_ADDRESS')
+      }
+      map && map.panTo(new window.google.maps.LatLng(center.lat, center.lng))
+    })
+  }
+
+  /**
+   * Function to return distance between two locations
+   * @param {google location} loc1
+   * @param {*google location} loc2
+   */
+  const validateResult = (map, marker, curPos) => {
+    const loc1 = new window.google.maps.LatLng(curPos.lat(), curPos.lng())
+    const loc2 = new window.google.maps.LatLng(location.lat, location.lng)
+
+    const distance = window.google.maps.geometry.spherical.computeDistanceBetween(loc1, loc2)
+
+    if (!maxLimitLocation) {
+      geocodePosition(curPos, map, marker)
+      return
+    }
+
+    if (distance <= maxLimitLocation) {
+      geocodePosition(curPos)
+    } else {
+      marker.setPosition(center)
+      map.panTo(new window.google.maps.LatLng(center.lat, center.lng))
+      setErrors && setErrors('ERROR_MAX_LIMIT_LOCATION')
+    }
+  }
+
   useEffect(() => {
     if (googleReady) {
-      const coordinates = { lat: location.lat, lng: location.lng }
       const map = new window.google.maps.Map(divRef.current, {
         zoom: location.zoom || mapControls.defaultZoom,
-        center: coordinates,
+        center,
         zoomControl: mapControls?.zoomControl,
         streetViewControl: mapControls?.streetViewControl,
         fullscreenControl: mapControls?.fullscreenControl,
@@ -63,18 +129,31 @@ export const GoogleMaps = (props) => {
       })
 
       let marker = null
+      setGoogleMap(map)
+
       if (locations) {
-        setGoogleMap(map)
         generateMarkers(map)
       } else {
         marker = new window.google.maps.Marker({
-          position: new window.google.maps.LatLng(coordinates.lat, coordinates.lng),
+          position: new window.google.maps.LatLng(center.lat, center.lng),
           map,
           draggable: true
         })
-        marker.addListener('mouseup', marker => {
-          handleChangePosition(marker.latLng)
+
+        window.google.maps.event.addListener(marker, 'dragend', () => validateResult(map, marker, marker.getPosition()))
+
+        window.google.maps.event.addListener(map, 'drag', () => marker.setPosition(map.getCenter()))
+
+        window.google.maps.event.addListener(map, 'dragend', () => {
+          marker.setPosition(map.getCenter())
+          validateResult(map, marker, map.getCenter())
         })
+      }
+
+      return () => {
+        window.google.maps.event.clearListeners(marker, 'dragend')
+        window.google.maps.event.clearListeners(map, 'drag')
+        window.google.maps.event.clearListeners(map, 'dragend')
       }
     }
   }, [googleReady])
@@ -102,6 +181,14 @@ GoogleMaps.propTypes = {
    * UI Component, this must be containt all graphic elements and use parent props
    */
   UIComponent: PropTypes.elementType,
+  /**
+   * maxLimitLocation, max value to set position
+   */
+  maxLimitLocation: PropTypes.number,
+  /**
+   * handleChangeAddressMap, function to set address when pin is moved
+   */
+  handleChangeAddressMap: PropTypes.func,
   /**
    * Components types before [PUT HERE COMPONENT NAME]
    * Array of type components, the parent props will pass to these components
