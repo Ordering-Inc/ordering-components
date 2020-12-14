7f850d63b701a85d774c1284041a158f0ea1182b
Jesus Perez <jesusdpch00@gmail.com>
2020-12-14 19:06:52 -0400

    [FIX] import

--- src/components/FacebookLoginButton/index.js
+++ src/components/FacebookLoginButton/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { useApi } from 'ordering-components'
+import { useApi } from '../../contexts/ApiContext'
 
 /**
  * Component to manage Facebook login behavior without UI component
