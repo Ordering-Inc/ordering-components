2acba07b7d4c8d82baf5ee1b91418089d576aa30
Jorge Diaz <jorge@ordering.co>
2020-12-22 09:09:48 -0500

    improvement analitycs

--- src/components/Analitycs/index.js
+++ src/components/Analitycs/index.js
@@ -35,6 +35,7 @@ export const Analytics = (props) => {
     js.onload = function () {
       setAnalyticsReady(true)
       window.ga('create', trackId)
+      window.ga('require', 'ec')
       window.ga('set', 'page', window.location.pathname)
       window.ga('send', 'pageview')
     }
@@ -50,18 +51,58 @@ export const Analytics = (props) => {
    * @param {String} pageName
    */
   const handlechangeView = (pageName) => {
-    console.log('Data', pageName)
     window.ga('set', 'page', pageName)
     window.ga('send', 'pageview')
+  }
+  const handleClickProduct = (product) => {
+    window.ga('ec:addProduct', {
+      id: product.id,
+      name: product.name,
+      category: product.category_id,
+      price: product.price
+    })
+    window.ga('ec:setAction', 'click')
+    window.ga('send', 'event', 'UI', 'click', 'add to cart')
+  }
+  const handleProductAdded = (product) => {
+    window.ga('ec:addProduct', {
+      id: product.id,
+      name: product.name,
+      category: product.category_id,
+      price: product.price,
+      quantity: product.quantity
+    })
+    window.ga('ec:setAction', 'add')
+    window.ga('send', 'event', 'UI', 'click', 'add to cart')
+  }
+  const handleLogin = (data) => {
+    window.ga('set', 'userId', data.id)
+  }
+  const handleOrderPlaced = (order) => {
+    window.ga('ec:setAction', 'purchase', { // Transaction details are provided in an actionFieldObject.
+      id: order.id, // (Required) Transaction id (string).
+      affiliation: order.business?.name, // Affiliation (string).
+      revenue: order.total, // Revenue (number).
+      tax: order.tax_total, // Tax (number).
+      shipping: order.delivery_zone_price // Shipping (number).
+    })
   }
   useEffect(() => {
     console.log('Analytic Ready')
     if (analyticsReady && window.ga) {
       events.on('change_view', handlechangeView)
+      events.on('userLogin', handleLogin)
+      events.on('product_clicked', handleClickProduct)
+      events.on('product_added', handleProductAdded)
+      events.on('order_placed', handleOrderPlaced)
     }
     return () => {
       if (analyticsReady && window.ga) {
         events.off('change_view', handlechangeView)
+        events.off('userLogin', handleLogin)
+        events.off('product_clicked', handleClickProduct)
+        events.off('product_added', handleProductAdded)
+        events.on('order_placed', handleOrderPlaced)
       }
     }
   }, [analyticsReady])
