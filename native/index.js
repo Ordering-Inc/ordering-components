fb171106091896e8898df9aebec0b13f496812e4
carlos quiroz <carloseqv29596@gmail.com>
2021-01-12 10:12:33 -0400

    [FEAT] rename native folder

---
+++ native/index.js
@@ -0,0 +1,167 @@
+import { AddressDetails } from '../src/components/AddressDetails'
+import { AddressForm } from '../src/components/AddressForm'
+import { AddressList } from '../src/components/AddressList'
+import { BusinessAndProductList } from '../src/components/BusinessAndProductList'
+import { BusinessBasicInformation } from '../src/components/BusinessBasicInformation'
+import { BusinessController } from '../src/components/BusinessController'
+import { BusinessList } from '../src/components/BusinessList'
+import { BusinessFeaturedProducts } from '../src/components/BusinessFeaturedProducts'
+import { BusinessInformation } from '../src/components/BusinessInformation'
+import { BusinessProductsCategories } from '../src/components/BusinessProductsCategories'
+import { BusinessProductsSearch } from '../src/components/BusinessProductsSearch'
+import { BusinessReviews } from '../src/components/BusinessReviews'
+import { BusinessSortControl } from '../src/components/BusinessSortControl'
+import { BusinessTypeFilter } from '../src/components/BusinessTypeFilter'
+import { CardForm } from '../src/components/StripeElementsForm/CardForm'
+import { Cart } from '../src/components/Cart'
+import { Checkout } from '../src/components/Checkout'
+import { CmsContent } from '../src/components/CmsContent'
+import { CouponControl } from '../src/components/CouponControl'
+import { DragAndDrop } from '../src/components/DragAndDrop'
+import { DriverTips } from '../src/components/DriverTips'
+import { ExamineClick } from '../src/components/ExamineClick'
+import { FloatingButton } from '../src/components/FloatingButton'
+import { ForgotPasswordForm } from '../src/components/ForgotPasswordForm'
+import { LanguageSelector } from '../src/components/LanguageSelector'
+import { LoginForm } from '../src/components/LoginForm'
+import { LogoutAction } from '../src/components/LogoutAction'
+import { MainSearch } from '../src/components/MainSearch'
+import { MenuControl } from '../src/components/MenuControl'
+import { Messages } from '../src/components/Messages'
+import { MomentOption } from '../src/components/MomentOption'
+import { MyOrders } from '../src/components/MyOrders'
+import { MyOrdersList } from '../src/components/MyOrdersList'
+import { OrderList } from '../src/components/OrderList'
+import { OrderTypeControl } from '../src/components/OrderTypeControl'
+import { OrderDetails } from '../src/components/OrderDetails'
+import { OrderReview } from '../src/components/OrderReview'
+import { PaymentOptions } from '../src/components/PaymentOptions'
+import { PaymentOptionCash } from '../src/components/PaymentOptionCash'
+import { PaymentOptionStripe } from '../src/components/PaymentOptionStripe'
+import { PaymentOptionStripeDirect } from '../src/components/PaymentOptionStripeDirect'
+import { PaymentOptionStripeRedirect } from '../src/components/PaymentOptionStripeRedirect'
+
+import { ProductForm } from '../src/components/ProductForm'
+import { ProductImages } from '../src/components/ProductImages'
+import { ProductIngredient } from '../src/components/ProductIngredient'
+import { ProductOption } from '../src/components/ProductOption'
+import { ProductOptionSuboption } from '../src/components/ProductOptionSuboption'
+import { ProductsList } from '../src/components/ProductsList'
+import { ProductsListing } from '../src/components/ProductsListing'
+import { ResetPassword } from '../src/components/ResetPassword'
+import { SearchOptions } from '../src/components/SearchOptions'
+import { SignupForm } from '../src/components/SignupForm'
+import { SingleBusinessCard } from '../src/components/SingleBusinessCard'
+import { SingleOrderCard } from '../src/components/SingleOrderCard'
+import { SingleProductCard } from '../src/components/SingleProductCard'
+import { StripeElementsForm } from '../src/components/StripeElementsForm'
+import { StripeRedirectForm } from '../src/components/PaymentOptionStripeRedirect/StripeRedirectForm'
+import { UserFormDetails } from '../src/components/UserFormDetails'
+import { UpsellingPage } from '../src/components/UpsellingPage'
+
+import { ApiContext, ApiProvider, useApi } from '../src/contexts/ApiContext'
+import { BusinessContext, BusinessProvider, useBusiness } from '../src/contexts/BusinessContext'
+import { ConfigContext, ConfigProvider, useConfig } from '../src/contexts/ConfigContext'
+import { EventContext, EventProvider, useEvent } from '../src/contexts/EventContext'
+import { LanguageContext, LanguageProvider, useLanguage } from '../src/contexts/LanguageContext'
+import { OrderContext, OrderProvider, useOrder } from '../src/contexts/OrderContext'
+import { OrderingContext, OrderingProvider } from '../src/contexts/OrderingContext'
+import { SessionContext, SessionProvider, useSession } from '../src/contexts/SessionContext'
+import { UtilsContext, UtilsProviders, useUtils } from '../src/contexts/UtilsContext'
+import { ValidationFieldsContext, useValidationFields, ValidationFieldsProvider } from '../src/contexts/ValidationsFieldsContext'
+import { WebsocketContext, WebsocketProvider, useWebsocket } from '../src/contexts/WebsocketContext'
+
+export {
+  AddressDetails,
+  AddressForm,
+  AddressList,
+  BusinessAndProductList,
+  BusinessBasicInformation,
+  BusinessController,
+  BusinessList,
+  BusinessFeaturedProducts,
+  BusinessInformation,
+  BusinessProductsCategories,
+  BusinessProductsSearch,
+  BusinessReviews,
+  BusinessSortControl,
+  BusinessTypeFilter,
+  CardForm,
+  Cart,
+  Checkout,
+  CmsContent,
+  CouponControl,
+  DragAndDrop,
+  DriverTips,
+  ExamineClick,
+  FloatingButton,
+  ForgotPasswordForm,
+  LanguageSelector,
+  LoginForm,
+  LogoutAction,
+  MainSearch,
+  MenuControl,
+  Messages,
+  MomentOption,
+  MyOrders,
+  MyOrdersList,
+  OrderList,
+  OrderTypeControl,
+  OrderDetails,
+  OrderReview,
+  PaymentOptions,
+  PaymentOptionCash,
+  PaymentOptionStripe,
+  PaymentOptionStripeDirect,
+  PaymentOptionStripeRedirect,
+  ProductForm,
+  ProductImages,
+  ProductIngredient,
+  ProductOption,
+  ProductOptionSuboption,
+  ProductsList,
+  ProductsListing,
+  ResetPassword,
+  SearchOptions,
+  SignupForm,
+  SingleBusinessCard,
+  SingleOrderCard,
+  SingleProductCard,
+  StripeElementsForm,
+  StripeRedirectForm,
+  UserFormDetails,
+  UpsellingPage,
+  // Contexts
+  ApiContext,
+  ApiProvider,
+  useApi,
+  BusinessContext,
+  BusinessProvider,
+  useBusiness,
+  ConfigContext,
+  ConfigProvider,
+  useConfig,
+  EventContext,
+  EventProvider,
+  useEvent,
+  LanguageContext,
+  LanguageProvider,
+  useLanguage,
+  OrderContext,
+  OrderProvider,
+  useOrder,
+  OrderingContext,
+  OrderingProvider,
+  SessionContext,
+  SessionProvider,
+  useSession,
+  UtilsContext,
+  UtilsProviders,
+  useUtils,
+  WebsocketContext,
+  WebsocketProvider,
+  useWebsocket,
+  ValidationFieldsContext,
+  useValidationFields,
+  ValidationFieldsProvider
+}
