1c7d9923ac68621a2a39c23926af56b812f533dc
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-01-25 21:13:35 -0400

    [FEAT] improve paypal component (#239)
    * [FEAT] improve paypal component
    
    * [FEAT] export payapl component

--- example/views/PaymentOptionPaypalExample/index.js
+++ example/views/PaymentOptionPaypalExample/index.js
@@ -3,21 +3,23 @@ import React from 'react'
 import { PaymentOptionPaypalUI } from '../../components/PaymentOptionPaypalUI'
 import { PaymentOptionPaypal } from '../../../src/components/PaymentOptionPaypal'
 import { TestComponent } from '../../components/TestComponent'
+import { useSession } from '../../../src/contexts/SessionContext'
 
 export const PaymentOptionPaypalExample = () => {
+  const [{ token, loading }] = useSession()
   const props = {
     /**
      * UI Component, this must be containt all graphic elements and use parent props
      */
     UIComponent: PaymentOptionPaypalUI,
     /**
-     * Amount to charge on paypal method
+     * User token
      */
-    amount: '1.00',
+    token,
     /**
      * paypal ClientID
      */
-    clientID: '',
+    clientId: 'AULNRFhxjY5BzJ_uXoiB6R4hM1SNwKGowkphqzGuVpACCGF-VXeKYEoyftxycsCSXnsl1x2lp7Ygjnkr',
     /**
      * style, some properties to show paypal button, check out the docs https://developer.paypal.com/docs/archive/checkout/how-to/customize-button/ for a complete documentation
      */
@@ -28,29 +30,42 @@ export const PaymentOptionPaypalExample = () => {
       label: 'paypal'
     },
     /**
+     * Body params to get payment and place order
+     */
+    body: {
+      paymethod_id: 3,
+      amount: 7.57,
+      delivery_zone_id: 43,
+      cartUuid: 'f97e12de-8090-49a2-a4ae-ff060498d432'
+    },
+    /**
+     * Message to show when user hasn't token
+     */
+    noAuthMessage: !token ? 'Sorry, you need to login to use this method' : null,
+    /**
      * Method to get success response from paypal button
      */
     handlerChangePaypal: null,
     /**
-     * Components types before payment option paypal
+     * Components types before Payment options paypal
      * Array of type components, the parent props will pass to these components
      */
     beforeComponents: [TestComponent],
     /**
-     * Components types after payment option paypal
+     * Components types after Payment options paypal
      * Array of type components, the parent props will pass to these components
      */
     afterComponents: [TestComponent],
     /**
-     * Elements before payment option paypal
+     * Elements before Payment options paypal
      * Array of HTML/Components elements, these components will not get the parent props
      */
     beforeElements: [<p key>Test Element Before</p>],
     /**
-     * Elements after payment option paypal
+     * Elements after Payment options paypal
      * Array of HTML/Components elements, these components will not get the parent props
      */
     afterElements: [<p key>Test Element After</p>]
   }
-  return <PaymentOptionPaypal {...props} />
+  return !loading && token && <PaymentOptionPaypal {...props} />
 }
