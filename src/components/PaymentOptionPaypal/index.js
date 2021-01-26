eb318aff303b37ffc7946976e0c47e84a01a4909
carlos quiroz <carlos.q@ordering.co>
2021-01-26 17:04:55 -0400

    [FEAT] update payment method paypal component

--- src/components/PaymentOptionPaypal/index.js
+++ src/components/PaymentOptionPaypal/index.js
@@ -1,28 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
-import { useApi } from '../../contexts/ApiContext'
+import { useOrder } from '../../contexts/OrderContext'
 
 /**
  * Component to manage payment option paypal behavior without UI component
  */
 export const PaymentOptionPaypal = (props) => {
   const {
-    token,
     body,
     clientId,
     btnStyle,
     UIComponent,
-    onButtonReady
+    handlerChangePaypal
   } = props
 
-  const [ordering] = useApi()
+  const [, { confirmCart, placeCart }] = useOrder()
   const [isSdkReady, setIsSdkReady] = useState(false)
 
-  /**
-   * Method to create script for paypal sdk
-   */
-  const addPaypalSdk = () => {
+  useEffect(() => {
+    if (!clientId) return
+    if (window?.paypal) {
+      setIsSdkReady(true)
+      return
+    }
     const script = document.createElement('script')
     script.type = 'text/javascript'
     script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
@@ -35,61 +36,38 @@ export const PaymentOptionPaypal = (props) => {
     }
 
     document.body.appendChild(script)
-  }
 
-  useEffect(() => {
-    if (window !== undefined && window.paypal === undefined) {
-      addPaypalSdk()
-    } else if (window !== undefined && window.paypal !== undefined && onButtonReady) {
-      onButtonReady()
+    return () => {
+      script.onload = null
     }
-  }, [])
+  }, [clientId])
 
-  const createOrder = (data, actions) => {
+  const createOrder = async (data, actions) => {
     const params = {
       paymethod_id: body.paymethod_id,
       amount: body.amount,
       delivery_zone_id: body.delivery_zone_id
     }
-    const url = `${ordering.root}/carts/${body.cartUuid}/place`
-    return fetch(url, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-      body: JSON.stringify(params)
-    }).then(function (res) {
-      return res.json()
-    }).then(function (res) {
-      if (res.error) {
-        console.log(res.result)
-        return null
-      } else {
-        return res.result.pay_reference
-      }
-    })
+
+    const { error, result } = await placeCart(body.cartUuid, params)
+    if (error) {
+      return null
+    }
+    return result.pay_reference
   }
 
-  const onApprove = (data, actions) => {
-    const url = `${ordering.root}/carts/${body.cartUuid}/confirm`
-    return fetch(url, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      }
-    }).then(function (res) {
-      return res.json()
-    }).then(function (res) {
-      console.log(res)
-      return res
-    })
+  const onApprove = async (data, actions) => {
+    const { error, result } = await confirmCart(body.cartUuid)
+    if (error) {
+      return null
+    }
+    handlerChangePaypal && handlerChangePaypal(result.order.uuid)
+    return result
   }
 
-  const onError = (error) => {
-    console.log(error)
-    return error
+  const onFail = async (data) => {
+    const { result } = await confirmCart(body.cartUuid)
+    return result
   }
 
   /**
@@ -109,7 +87,8 @@ export const PaymentOptionPaypal = (props) => {
         paypalButtonProps={{
           createOrder,
           onApprove,
-          onError,
+          onError: onFail,
+          onCancel: onFail,
           style: btnStyle
         }}
       />
