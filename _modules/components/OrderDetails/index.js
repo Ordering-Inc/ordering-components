381f47e554f6b9311abe06149ce5de886950247e
Jesus Perez <jesus@ordering.co>
2021-02-04 20:25:38 -0400

    build

--- _modules/components/OrderDetails/index.js
+++ _modules/components/OrderDetails/index.js
@@ -103,6 +103,11 @@ var OrderDetails = function OrderDetails(props) {
       driverLocation = _useState8[0],
       setDriverLocation = _useState8[1];
 
+  var _useState9 = (0, _react.useState)(false),
+      _useState10 = _slicedToArray(_useState9, 2),
+      isReadMessages = _useState10[0],
+      setIsReadMessages = _useState10[1];
+
   var propsToFetch = ['header', 'slug'];
   /**
    * Method to format a price number
@@ -318,7 +323,7 @@ var OrderDetails = function OrderDetails(props) {
     var _ref5 = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee4() {
       var _messages$messages, _messages$messages2;
 
-      var messageId, _orderState$order5, response;
+      var messageId, _orderState$order5;
 
       return _regenerator.default.wrap(function _callee4$(_context4) {
         while (1) {
@@ -336,22 +341,21 @@ var OrderDetails = function OrderDetails(props) {
               });
 
             case 4:
-              response = _context4.sent;
-              console.log(response);
-              _context4.next = 11;
+              setIsReadMessages(true);
+              _context4.next = 10;
               break;
 
-            case 8:
-              _context4.prev = 8;
+            case 7:
+              _context4.prev = 7;
               _context4.t0 = _context4["catch"](1);
               console.log(_context4.t0.message);
 
-            case 11:
+            case 10:
             case "end":
               return _context4.stop();
           }
         }
-      }, _callee4, null, [[1, 8]]);
+      }, _callee4, null, [[1, 7]]);
     }));
 
     return function readMessages() {
@@ -417,7 +421,8 @@ var OrderDetails = function OrderDetails(props) {
     handlerSubmit: handlerSubmitSpotNumber,
     messages: messages,
     setMessages: setMessages,
-    readMessages: readMessages
+    readMessages: readMessages,
+    isReadMessages: isReadMessages
   })));
 };
 
