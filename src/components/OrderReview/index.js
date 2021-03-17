8d0b52ad995bcffaaca74e80f12a35ec3ffb0e2f
Jesus Perez <jesus@ordering.co>
2021-03-16 20:41:43 -0400

    [FIX] formState response

--- src/components/OrderReview/index.js
+++ src/components/OrderReview/index.js
@@ -34,8 +34,9 @@ export const OrderReview = (props) => {
         },
         body: JSON.stringify(body)
       })
-      onSaveReview && onSaveReview(response.content)
-      setFormState({ loading: false, result: response.content })
+      const res = await response.json()
+      onSaveReview && onSaveReview(response)
+      setFormState({ loading: false, result: res.result })
     } catch (err) {
       setFormState({
         result: {
