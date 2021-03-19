6b12f5e72f7376cf27cafabd6c7794dce3b6eb19
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-03-19 13:19:08 -0400

    include data for custom methods (#304)

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -45,7 +45,7 @@ export const LoginForm = (props) => {
    */
   const handleLoginClick = async (values) => {
     if (handleCustomLogin) {
-      handleCustomLogin(values, loginTab)
+      handleCustomLogin(values || credentials, loginTab)
       return
     }
 
