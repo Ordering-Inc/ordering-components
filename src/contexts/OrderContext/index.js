aad0756c6091efa8732fef099b9bf33962b753e9
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-07-14 13:05:06 -0700

    feat: add theme for context (#424)

--- src/contexts/OrderContext/index.js
+++ src/contexts/OrderContext/index.js
@@ -22,7 +22,7 @@ export const OrderContext = createContext()
  * This provider has a reducer for manage order state
  * @param {props} props
  */
-export const OrderProvider = ({ Alert, children, strategy, isAlsea }) => {
+export const OrderProvider = ({ Alert, children, strategy, isAlsea, theme }) => {
   const [confirmAlert, setConfirm] = useState({ show: false })
   const [alert, setAlert] = useState({ show: false })
   const [ordering] = useApi()
@@ -780,6 +780,7 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea }) => {
       {
         Alert && (
           <Alert
+            colors={theme?.colors}
             open={alert.show}
             title={t('ERROR', 'Error')}
             onAccept={() => setAlert({ show: false })}
