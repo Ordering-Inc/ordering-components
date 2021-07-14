fe708fcfc5a1968ad178a38a634e24e59705739b
Jesus Perez <jesusdpch00@gmail.com>
2021-07-13 22:30:31 -0400

    build

---
+++ _modules/native/index.js
@@ -0,0 +1,691 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AddressDetails", {
+  enumerable: true,
+  get: function get() {
+    return _AddressDetails.AddressDetails;
+  }
+});
+Object.defineProperty(exports, "AddressForm", {
+  enumerable: true,
+  get: function get() {
+    return _AddressForm.AddressForm;
+  }
+});
+Object.defineProperty(exports, "AddressList", {
+  enumerable: true,
+  get: function get() {
+    return _AddressList.AddressList;
+  }
+});
+Object.defineProperty(exports, "BusinessAndProductList", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessAndProductList.BusinessAndProductList;
+  }
+});
+Object.defineProperty(exports, "BusinessBasicInformation", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessBasicInformation.BusinessBasicInformation;
+  }
+});
+Object.defineProperty(exports, "BusinessController", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessController.BusinessController;
+  }
+});
+Object.defineProperty(exports, "BusinessList", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessList.BusinessList;
+  }
+});
+Object.defineProperty(exports, "BusinessInformation", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessInformation.BusinessInformation;
+  }
+});
+Object.defineProperty(exports, "BusinessProductsCategories", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessProductsCategories.BusinessProductsCategories;
+  }
+});
+Object.defineProperty(exports, "BusinessProductsSearch", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessProductsSearch.BusinessProductsSearch;
+  }
+});
+Object.defineProperty(exports, "BusinessReviews", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessReviews.BusinessReviews;
+  }
+});
+Object.defineProperty(exports, "BusinessSortControl", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessSortControl.BusinessSortControl;
+  }
+});
+Object.defineProperty(exports, "BusinessTypeFilter", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessTypeFilter.BusinessTypeFilter;
+  }
+});
+Object.defineProperty(exports, "CardForm", {
+  enumerable: true,
+  get: function get() {
+    return _CardForm.CardForm;
+  }
+});
+Object.defineProperty(exports, "Cart", {
+  enumerable: true,
+  get: function get() {
+    return _Cart.Cart;
+  }
+});
+Object.defineProperty(exports, "Checkout", {
+  enumerable: true,
+  get: function get() {
+    return _Checkout.Checkout;
+  }
+});
+Object.defineProperty(exports, "CmsContent", {
+  enumerable: true,
+  get: function get() {
+    return _CmsContent.CmsContent;
+  }
+});
+Object.defineProperty(exports, "CouponControl", {
+  enumerable: true,
+  get: function get() {
+    return _CouponControl.CouponControl;
+  }
+});
+Object.defineProperty(exports, "DragAndDrop", {
+  enumerable: true,
+  get: function get() {
+    return _DragAndDrop.DragAndDrop;
+  }
+});
+Object.defineProperty(exports, "DriverTips", {
+  enumerable: true,
+  get: function get() {
+    return _DriverTips.DriverTips;
+  }
+});
+Object.defineProperty(exports, "ExamineClick", {
+  enumerable: true,
+  get: function get() {
+    return _ExamineClick.ExamineClick;
+  }
+});
+Object.defineProperty(exports, "FloatingButton", {
+  enumerable: true,
+  get: function get() {
+    return _FloatingButton.FloatingButton;
+  }
+});
+Object.defineProperty(exports, "ForgotPasswordForm", {
+  enumerable: true,
+  get: function get() {
+    return _ForgotPasswordForm.ForgotPasswordForm;
+  }
+});
+Object.defineProperty(exports, "LanguageSelector", {
+  enumerable: true,
+  get: function get() {
+    return _LanguageSelector.LanguageSelector;
+  }
+});
+Object.defineProperty(exports, "LoginForm", {
+  enumerable: true,
+  get: function get() {
+    return _LoginForm.LoginForm;
+  }
+});
+Object.defineProperty(exports, "LogoutAction", {
+  enumerable: true,
+  get: function get() {
+    return _LogoutAction.LogoutAction;
+  }
+});
+Object.defineProperty(exports, "MainSearch", {
+  enumerable: true,
+  get: function get() {
+    return _MainSearch.MainSearch;
+  }
+});
+Object.defineProperty(exports, "MenuControl", {
+  enumerable: true,
+  get: function get() {
+    return _MenuControl.MenuControl;
+  }
+});
+Object.defineProperty(exports, "Messages", {
+  enumerable: true,
+  get: function get() {
+    return _Messages.Messages;
+  }
+});
+Object.defineProperty(exports, "MomentOption", {
+  enumerable: true,
+  get: function get() {
+    return _MomentOption.MomentOption;
+  }
+});
+Object.defineProperty(exports, "MyOrders", {
+  enumerable: true,
+  get: function get() {
+    return _MyOrders.MyOrders;
+  }
+});
+Object.defineProperty(exports, "MyOrdersList", {
+  enumerable: true,
+  get: function get() {
+    return _MyOrdersList.MyOrdersList;
+  }
+});
+Object.defineProperty(exports, "OrderList", {
+  enumerable: true,
+  get: function get() {
+    return _OrderList.OrderList;
+  }
+});
+Object.defineProperty(exports, "OrderTypeControl", {
+  enumerable: true,
+  get: function get() {
+    return _OrderTypeControl.OrderTypeControl;
+  }
+});
+Object.defineProperty(exports, "OrderDetails", {
+  enumerable: true,
+  get: function get() {
+    return _OrderDetails.OrderDetails;
+  }
+});
+Object.defineProperty(exports, "OrderReview", {
+  enumerable: true,
+  get: function get() {
+    return _OrderReview.OrderReview;
+  }
+});
+Object.defineProperty(exports, "PaymentOptions", {
+  enumerable: true,
+  get: function get() {
+    return _PaymentOptions.PaymentOptions;
+  }
+});
+Object.defineProperty(exports, "PaymentOptionCash", {
+  enumerable: true,
+  get: function get() {
+    return _PaymentOptionCash.PaymentOptionCash;
+  }
+});
+Object.defineProperty(exports, "PaymentOptionStripe", {
+  enumerable: true,
+  get: function get() {
+    return _PaymentOptionStripe.PaymentOptionStripe;
+  }
+});
+Object.defineProperty(exports, "PaymentOptionStripeDirect", {
+  enumerable: true,
+  get: function get() {
+    return _PaymentOptionStripeDirect.PaymentOptionStripeDirect;
+  }
+});
+Object.defineProperty(exports, "PaymentOptionStripeRedirect", {
+  enumerable: true,
+  get: function get() {
+    return _PaymentOptionStripeRedirect.PaymentOptionStripeRedirect;
+  }
+});
+Object.defineProperty(exports, "ProductForm", {
+  enumerable: true,
+  get: function get() {
+    return _ProductForm.ProductForm;
+  }
+});
+Object.defineProperty(exports, "ProductImages", {
+  enumerable: true,
+  get: function get() {
+    return _ProductImages.ProductImages;
+  }
+});
+Object.defineProperty(exports, "ProductIngredient", {
+  enumerable: true,
+  get: function get() {
+    return _ProductIngredient.ProductIngredient;
+  }
+});
+Object.defineProperty(exports, "ProductOption", {
+  enumerable: true,
+  get: function get() {
+    return _ProductOption.ProductOption;
+  }
+});
+Object.defineProperty(exports, "ProductOptionSuboption", {
+  enumerable: true,
+  get: function get() {
+    return _ProductOptionSuboption.ProductOptionSuboption;
+  }
+});
+Object.defineProperty(exports, "ProductsList", {
+  enumerable: true,
+  get: function get() {
+    return _ProductsList.ProductsList;
+  }
+});
+Object.defineProperty(exports, "ProductsListing", {
+  enumerable: true,
+  get: function get() {
+    return _ProductsListing.ProductsListing;
+  }
+});
+Object.defineProperty(exports, "ResetPassword", {
+  enumerable: true,
+  get: function get() {
+    return _ResetPassword.ResetPassword;
+  }
+});
+Object.defineProperty(exports, "SearchOptions", {
+  enumerable: true,
+  get: function get() {
+    return _SearchOptions.SearchOptions;
+  }
+});
+Object.defineProperty(exports, "SignupForm", {
+  enumerable: true,
+  get: function get() {
+    return _SignupForm.SignupForm;
+  }
+});
+Object.defineProperty(exports, "SingleBusinessCard", {
+  enumerable: true,
+  get: function get() {
+    return _SingleBusinessCard.SingleBusinessCard;
+  }
+});
+Object.defineProperty(exports, "SingleOrderCard", {
+  enumerable: true,
+  get: function get() {
+    return _SingleOrderCard.SingleOrderCard;
+  }
+});
+Object.defineProperty(exports, "SingleProductCard", {
+  enumerable: true,
+  get: function get() {
+    return _SingleProductCard.SingleProductCard;
+  }
+});
+Object.defineProperty(exports, "StripeElementsForm", {
+  enumerable: true,
+  get: function get() {
+    return _StripeElementsForm.StripeElementsForm;
+  }
+});
+Object.defineProperty(exports, "StripeRedirectForm", {
+  enumerable: true,
+  get: function get() {
+    return _StripeRedirectForm.StripeRedirectForm;
+  }
+});
+Object.defineProperty(exports, "UserFormDetails", {
+  enumerable: true,
+  get: function get() {
+    return _UserFormDetails.UserFormDetails;
+  }
+});
+Object.defineProperty(exports, "UpsellingPage", {
+  enumerable: true,
+  get: function get() {
+    return _UpsellingPage.UpsellingPage;
+  }
+});
+Object.defineProperty(exports, "ApiContext", {
+  enumerable: true,
+  get: function get() {
+    return _ApiContext.ApiContext;
+  }
+});
+Object.defineProperty(exports, "ApiProvider", {
+  enumerable: true,
+  get: function get() {
+    return _ApiContext.ApiProvider;
+  }
+});
+Object.defineProperty(exports, "useApi", {
+  enumerable: true,
+  get: function get() {
+    return _ApiContext.useApi;
+  }
+});
+Object.defineProperty(exports, "BusinessContext", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessContext.BusinessContext;
+  }
+});
+Object.defineProperty(exports, "BusinessProvider", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessContext.BusinessProvider;
+  }
+});
+Object.defineProperty(exports, "useBusiness", {
+  enumerable: true,
+  get: function get() {
+    return _BusinessContext.useBusiness;
+  }
+});
+Object.defineProperty(exports, "ConfigContext", {
+  enumerable: true,
+  get: function get() {
+    return _ConfigContext.ConfigContext;
+  }
+});
+Object.defineProperty(exports, "ConfigProvider", {
+  enumerable: true,
+  get: function get() {
+    return _ConfigContext.ConfigProvider;
+  }
+});
+Object.defineProperty(exports, "useConfig", {
+  enumerable: true,
+  get: function get() {
+    return _ConfigContext.useConfig;
+  }
+});
+Object.defineProperty(exports, "EventContext", {
+  enumerable: true,
+  get: function get() {
+    return _EventContext.EventContext;
+  }
+});
+Object.defineProperty(exports, "EventProvider", {
+  enumerable: true,
+  get: function get() {
+    return _EventContext.EventProvider;
+  }
+});
+Object.defineProperty(exports, "useEvent", {
+  enumerable: true,
+  get: function get() {
+    return _EventContext.useEvent;
+  }
+});
+Object.defineProperty(exports, "LanguageContext", {
+  enumerable: true,
+  get: function get() {
+    return _LanguageContext.LanguageContext;
+  }
+});
+Object.defineProperty(exports, "LanguageProvider", {
+  enumerable: true,
+  get: function get() {
+    return _LanguageContext.LanguageProvider;
+  }
+});
+Object.defineProperty(exports, "useLanguage", {
+  enumerable: true,
+  get: function get() {
+    return _LanguageContext.useLanguage;
+  }
+});
+Object.defineProperty(exports, "OrderContext", {
+  enumerable: true,
+  get: function get() {
+    return _OrderContext.OrderContext;
+  }
+});
+Object.defineProperty(exports, "OrderProvider", {
+  enumerable: true,
+  get: function get() {
+    return _OrderContext.OrderProvider;
+  }
+});
+Object.defineProperty(exports, "useOrder", {
+  enumerable: true,
+  get: function get() {
+    return _OrderContext.useOrder;
+  }
+});
+Object.defineProperty(exports, "SessionContext", {
+  enumerable: true,
+  get: function get() {
+    return _SessionContext.SessionContext;
+  }
+});
+Object.defineProperty(exports, "SessionProvider", {
+  enumerable: true,
+  get: function get() {
+    return _SessionContext.SessionProvider;
+  }
+});
+Object.defineProperty(exports, "useSession", {
+  enumerable: true,
+  get: function get() {
+    return _SessionContext.useSession;
+  }
+});
+Object.defineProperty(exports, "UtilsContext", {
+  enumerable: true,
+  get: function get() {
+    return _UtilsContext.UtilsContext;
+  }
+});
+Object.defineProperty(exports, "UtilsProviders", {
+  enumerable: true,
+  get: function get() {
+    return _UtilsContext.UtilsProviders;
+  }
+});
+Object.defineProperty(exports, "useUtils", {
+  enumerable: true,
+  get: function get() {
+    return _UtilsContext.useUtils;
+  }
+});
+Object.defineProperty(exports, "ValidationFieldsContext", {
+  enumerable: true,
+  get: function get() {
+    return _ValidationsFieldsContext.ValidationFieldsContext;
+  }
+});
+Object.defineProperty(exports, "useValidationFields", {
+  enumerable: true,
+  get: function get() {
+    return _ValidationsFieldsContext.useValidationFields;
+  }
+});
+Object.defineProperty(exports, "ValidationFieldsProvider", {
+  enumerable: true,
+  get: function get() {
+    return _ValidationsFieldsContext.ValidationFieldsProvider;
+  }
+});
+Object.defineProperty(exports, "WebsocketContext", {
+  enumerable: true,
+  get: function get() {
+    return _WebsocketContext.WebsocketContext;
+  }
+});
+Object.defineProperty(exports, "WebsocketProvider", {
+  enumerable: true,
+  get: function get() {
+    return _WebsocketContext.WebsocketProvider;
+  }
+});
+Object.defineProperty(exports, "useWebsocket", {
+  enumerable: true,
+  get: function get() {
+    return _WebsocketContext.useWebsocket;
+  }
+});
+Object.defineProperty(exports, "OrderingContext", {
+  enumerable: true,
+  get: function get() {
+    return _OrderingContext.OrderingContext;
+  }
+});
+Object.defineProperty(exports, "OrderingProvider", {
+  enumerable: true,
+  get: function get() {
+    return _OrderingContext.OrderingProvider;
+  }
+});
+Object.defineProperty(exports, "CODES", {
+  enumerable: true,
+  get: function get() {
+    return _codeNumbers.CODES;
+  }
+});
+
+var _AddressDetails = require("../src/components/AddressDetails");
+
+var _AddressForm = require("../src/components/AddressForm");
+
+var _AddressList = require("../src/components/AddressList");
+
+var _BusinessAndProductList = require("../src/components/BusinessAndProductList");
+
+var _BusinessBasicInformation = require("../src/components/BusinessBasicInformation");
+
+var _BusinessController = require("../src/components/BusinessController");
+
+var _BusinessList = require("../src/components/BusinessList");
+
+var _BusinessInformation = require("../src/components/BusinessInformation");
+
+var _BusinessProductsCategories = require("../src/components/BusinessProductsCategories");
+
+var _BusinessProductsSearch = require("../src/components/BusinessProductsSearch");
+
+var _BusinessReviews = require("../src/components/BusinessReviews");
+
+var _BusinessSortControl = require("../src/components/BusinessSortControl");
+
+var _BusinessTypeFilter = require("../src/components/BusinessTypeFilter");
+
+var _CardForm = require("../src/components/StripeElementsForm/CardForm");
+
+var _Cart = require("../src/components/Cart");
+
+var _Checkout = require("../src/components/Checkout");
+
+var _CmsContent = require("../src/components/CmsContent");
+
+var _CouponControl = require("../src/components/CouponControl");
+
+var _DragAndDrop = require("../src/components/DragAndDrop");
+
+var _DriverTips = require("../src/components/DriverTips");
+
+var _ExamineClick = require("../src/components/ExamineClick");
+
+var _FloatingButton = require("../src/components/FloatingButton");
+
+var _ForgotPasswordForm = require("../src/components/ForgotPasswordForm");
+
+var _LanguageSelector = require("../src/components/LanguageSelector");
+
+var _LoginForm = require("../src/components/LoginForm");
+
+var _LogoutAction = require("../src/components/LogoutAction");
+
+var _MainSearch = require("../src/components/MainSearch");
+
+var _MenuControl = require("../src/components/MenuControl");
+
+var _Messages = require("../src/components/Messages");
+
+var _MomentOption = require("../src/components/MomentOption");
+
+var _MyOrders = require("../src/components/MyOrders");
+
+var _MyOrdersList = require("../src/components/MyOrdersList");
+
+var _OrderList = require("../src/components/OrderList");
+
+var _OrderTypeControl = require("../src/components/OrderTypeControl");
+
+var _OrderDetails = require("../src/components/OrderDetails");
+
+var _OrderReview = require("../src/components/OrderReview");
+
+var _PaymentOptions = require("../src/components/PaymentOptions");
+
+var _PaymentOptionCash = require("../src/components/PaymentOptionCash");
+
+var _PaymentOptionStripe = require("../src/components/PaymentOptionStripe");
+
+var _PaymentOptionStripeDirect = require("../src/components/PaymentOptionStripeDirect");
+
+var _PaymentOptionStripeRedirect = require("../src/components/PaymentOptionStripeRedirect");
+
+var _ProductForm = require("../src/components/ProductForm");
+
+var _ProductImages = require("../src/components/ProductImages");
+
+var _ProductIngredient = require("../src/components/ProductIngredient");
+
+var _ProductOption = require("../src/components/ProductOption");
+
+var _ProductOptionSuboption = require("../src/components/ProductOptionSuboption");
+
+var _ProductsList = require("../src/components/ProductsList");
+
+var _ProductsListing = require("../src/components/ProductsListing");
+
+var _ResetPassword = require("../src/components/ResetPassword");
+
+var _SearchOptions = require("../src/components/SearchOptions");
+
+var _SignupForm = require("../src/components/SignupForm");
+
+var _SingleBusinessCard = require("../src/components/SingleBusinessCard");
+
+var _SingleOrderCard = require("../src/components/SingleOrderCard");
+
+var _SingleProductCard = require("../src/components/SingleProductCard");
+
+var _StripeElementsForm = require("../src/components/StripeElementsForm");
+
+var _StripeRedirectForm = require("../src/components/PaymentOptionStripeRedirect/StripeRedirectForm");
+
+var _UserFormDetails = require("../src/components/UserFormDetails");
+
+var _UpsellingPage = require("../src/components/UpsellingPage");
+
+var _ApiContext = require("../src/contexts/ApiContext");
+
+var _BusinessContext = require("../src/contexts/BusinessContext");
+
+var _ConfigContext = require("../src/contexts/ConfigContext");
+
+var _EventContext = require("../src/contexts/EventContext");
+
+var _LanguageContext = require("../src/contexts/LanguageContext");
+
+var _OrderContext = require("../src/contexts/OrderContext");
+
+var _SessionContext = require("../src/contexts/SessionContext");
+
+var _UtilsContext = require("../src/contexts/UtilsContext");
+
+var _ValidationsFieldsContext = require("../src/contexts/ValidationsFieldsContext");
+
+var _WebsocketContext = require("../src/contexts/WebsocketContext");
+
+var _OrderingContext = require("./src/contexts/OrderingContext");
+
+var _codeNumbers = require("../src/constants/code-numbers");
