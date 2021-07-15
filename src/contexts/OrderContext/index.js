0e82182aef460d5b2ea58664786554b35ee3dee8
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2021-07-14 18:33:10 -0700

    Revert "feat: add theme for context (#424)" (#425)
    This reverts commit aad0756c6091efa8732fef099b9bf33962b753e9.

--- src/contexts/OrderContext/index.js
+++ src/contexts/OrderContext/index.js
@@ -22,7 +22,7 @@ export const OrderContext = createContext()
  * This provider has a reducer for manage order state
  * @param {props} props
  */
-export const OrderProvider = ({ Alert, children, strategy, isAlsea, theme }) => {
+export const OrderProvider = ({ Alert, children, strategy, isAlsea }) => {
   const [confirmAlert, setConfirm] = useState({ show: false })
   const [alert, setAlert] = useState({ show: false })
   const [ordering] = useApi()
@@ -780,7 +780,6 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea, theme }) => 
       {
         Alert && (
           <Alert
-            colors={theme?.colors}
             open={alert.show}
             title={t('ERROR', 'Error')}
             onAccept={() => setAlert({ show: false })}
