b2ec576ba7bf446a7bb6289894100e1e8e6a86e1
Yorman Rodriguez <yorman2414@gmail.com>
2021-07-18 17:44:11 -0400

    Feat: fetch businesses, cities, drivers and delivery types

---
+++ src/components/SearchOrders/index.js
@@ -0,0 +1,216 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes, { string, object } from 'prop-types';
+import { useSession } from '../../contexts/SessionContext';
+import { useConfig } from '../../contexts/ConfigContext';
+import { useApi } from '../../contexts/ApiContext';
+
+export const SearchOrders = (props) => {
+  const {
+    UIComponent,
+    fetchAll,
+    propsToFetch,
+    paramsToFetch,
+    conditionsToFetch,
+    conditionsConectorType,
+  } = props;
+
+  const [{ token }] = useSession();
+  const [{ loading, configs }] = useConfig();
+  const [ordering] = useApi();
+
+  const [businesses, setBusinesses] = useState({
+    data: [],
+    loading: false,
+    error: null,
+  });
+  const [cities, setCities] = useState({
+    data: [],
+    loading: false,
+    error: null,
+  });
+  const [drivers, setDrivers] = useState({
+    data: [],
+    loading: false,
+    error: null,
+  });
+  const [deliveryTypes, setDeliveryTypes] = useState({
+    data: [],
+    loading: false,
+    error: null,
+  });
+  const [paymentMethods, setPaymentMethods] = useState({
+    data: [],
+    loading: false,
+    error: null,
+  });
+
+  const where = {
+    conditions: conditionsToFetch,
+    conector: conditionsConectorType,
+  };
+
+  /**
+   * Method to get businesses from SDK
+   */
+  const getBusinesses = async () => {
+    setBusinesses({ ...businesses, loading: true, error: null });
+
+    try {
+      const {
+        content: { result, error },
+      } = await ordering
+        .businesses()
+        .select(propsToFetch)
+        .parameters(paramsToFetch)
+        .where(where)
+        .asDashboard()
+        .get();
+
+      if (!error) {
+        setBusinesses({ ...businesses, data: result, loading: false });
+      } else {
+        setBusinesses({ ...businesses, loading: false, error: result[0] });
+      }
+    } catch (err) {
+      setBusinesses({ ...businesses, loading: false, error: err.message });
+    }
+  };
+
+  /**
+   * Method to get cities from SDK
+   */
+  const getCities = async () => {
+    setCities({ ...cities, loading: true, error: null });
+
+    try {
+      const {
+        content: { result, error },
+      } = await ordering
+        .businesses()
+        .select(propsToFetch)
+        .where(where)
+        .asDashboard()
+        .get();
+
+      if (!error) {
+        setCities({
+          ...cities,
+          data: [
+            ...new Map(
+              result?.map((business) =>
+                business?.city ? [business.city?.id, business.city] : undefined
+              )
+            ).values(),
+          ],
+          loading: false,
+        });
+      } else {
+        setCities({ ...cities, loading: false, error: result[0] });
+      }
+    } catch (err) {
+      setCities({ ...cities, loading: false, error: err.message });
+    }
+  };
+
+  /**
+   * Method to get drivers from SDK
+   */
+  const getDrivers = async () => {
+    setDrivers({ ...drivers, loading: true, error: null });
+
+    try {
+      const {
+        content: { result, error },
+      } = await ordering.setAccessToken(token).users().get();
+
+      if (!error) {
+        setDrivers({ ...drivers, data: result, loading: false });
+      } else {
+        setDrivers({ ...drivers, loading: false, error: result[0] });
+      }
+    } catch (err) {
+      setDrivers({ ...drivers, loading: false, error: err.message });
+    }
+  };
+
+  // useEffect(() => {
+  //   setDeliveryTypes({ ...deliveryTypes, loading: true, error: null });
+  //   if (!loading) {
+  //     setDeliveryTypes({
+  //       ...deliveryTypes,
+  //       data: configs?.order_types_allowed,
+  //       loading: false
+  //     });
+  //   }
+  // }, [loading]);
+
+  useEffect(() => {
+    if (fetchAll) {
+      getBusinesses();
+      getDrivers();
+      getCities();
+    }
+  }, []);
+
+  return (
+    <>
+      {UIComponent && (
+        <UIComponent
+          {...props}
+          options={{ businesses, cities, drivers, deliveryTypes }}
+          deliveryTypes={configs?.order_types_allowed}
+          getBusinesses={getBusinesses}
+          getDrivers={getDrivers}
+          getCities={getCities}
+        />
+      )}
+    </>
+  );
+};
+
+SearchOrders.propTypes = {
+  /**
+   * UI Component, this must be containt all graphic elements and use parent props
+   */
+  UIComponent: PropTypes.elementType,
+
+  /**
+   * Boolean indicating whether to fetch all
+   */
+  fetchAll: PropTypes.bool,
+
+  /**
+   * Array of business props to fetch
+   */
+  propsToFetch: PropTypes.arrayOf(string),
+
+  /**
+   * Object of params to fetch
+   */
+  paramsToFetch: PropTypes.object,
+
+  /**
+   * Array of conditions to fetch
+   */
+  conditionsToFetch: PropTypes.arrayOf(object),
+
+  /**
+   * String that indicates the conector type of the conditions to fetch
+   */
+  conditionsConectorType: PropTypes.string,
+};
+
+SearchOrders.defaultProps = {
+  fetchAll: false,
+  propsToFetch: ['id', 'name', 'city'],
+  paramsToFetch: {
+    page: 1,
+    page_size: 25,
+    orderBy: 'name',
+    controlType: 'infinity',
+  },
+  conditionsToFetch: [
+    { attribute: 'enabled', value: { condition: '=', value: 'true' } },
+  ],
+  conditionsConectorType: 'AND',
+};
--- src/index.js
+++ src/index.js
@@ -1,90 +1,123 @@
-import { AddressDetails } from './components/AddressDetails'
-import { AddressForm } from './components/AddressForm'
-import { AddressList } from './components/AddressList'
-import { BaseComponent } from './components/BaseComponent'
-import { BusinessAndProductList } from './components/BusinessAndProductList'
-import { BusinessBasicInformation } from './components/BusinessBasicInformation'
-import { BusinessController } from './components/BusinessController'
-import { BusinessList } from './components/BusinessList'
-import { BusinessInformation } from './components/BusinessInformation'
-import { BusinessProductsCategories } from './components/BusinessProductsCategories'
-import { BusinessProductsSearch } from './components/BusinessProductsSearch'
-import { BusinessReviews } from './components/BusinessReviews'
-import { BusinessSortControl } from './components/BusinessSortControl'
-import { BusinessTypeFilter } from './components/BusinessTypeFilter'
-import { Cart } from './components/Cart'
-import { Checkout } from './components/Checkout'
-import { CouponControl } from './components/CouponControl'
-import { DragAndDrop } from './components/DragAndDrop'
-import { DriverTips } from './components/DriverTips'
-import { ExamineClick } from './components/ExamineClick'
-import { FacebookLoginButton } from './components/FacebookLoginButton'
-import { ForgotPasswordForm } from './components/ForgotPasswordForm'
-import { GoogleLoginButton } from './components/GoogleLoginButton'
-import { GoogleAutocompleteInput } from './components/GoogleAutocompleteInput'
-import { GoogleMaps, GoogleMapsMap } from './components/GoogleMaps'
-import { GpsButton, GoogleGpsButton } from './components/GpsButton'
-import { LanguageSelector } from './components/LanguageSelector'
-import { LoginForm } from './components/LoginForm'
-import { LogoutAction } from './components/LogoutAction'
-import { MainSearch } from './components/MainSearch'
-import { MenuControl } from './components/MenuControl'
-import { MomentOption } from './components/MomentOption'
-import { MyOrders } from './components/MyOrders'
-import { MyOrdersList } from './components/MyOrdersList'
-import { OrderList } from './components/OrderList'
-import { OrderTypeControl } from './components/OrderTypeControl'
-import { PaymentOptions } from './components/PaymentOptions'
-import { Popup } from './components/Popup'
-import { ProductComponent } from './components/ProductComponent'
-import { ProductForm } from './components/ProductForm'
-import { ProductShare } from './components/ProductShare'
-import { ProductImages } from './components/ProductImages'
-import { ProductIngredient } from './components/ProductIngredient'
-import { ProductOption } from './components/ProductOption'
-import { ProductOptionSuboption } from './components/ProductOptionSuboption'
-import { ProductsList } from './components/ProductsList'
-import { ProductsListing } from './components/ProductsListing'
-import { Messages } from './components/Messages'
-import { SearchOptions } from './components/SearchOptions'
-import { SignupForm } from './components/SignupForm'
-import { SingleBusinessCard } from './components/SingleBusinessCard'
-import { SingleOrderCard } from './components/SingleOrderCard'
-import { SingleProductCard } from './components/SingleProductCard'
-import { UserFormDetails } from './components/UserFormDetails'
-import { UpsellingPage } from './components/UpsellingPage'
-import { WrapperGoogleMaps } from './components/WrapperGoogleMaps'
-import { OrderDetails } from './components/OrderDetails'
-import { OrderReview } from './components/OrderReview'
-import { CmsContent } from './components/CmsContent'
-import { PhoneAutocomplete } from './components/PhoneAutocomplete'
-import { BusinessesMap } from './components/BusinessesMap'
-import { ConfigContext, ConfigProvider, useConfig } from './contexts/ConfigContext'
-import { LanguageContext, LanguageProvider, useLanguage } from './contexts/LanguageContext'
-import { OrderContext, OrderProvider, useOrder } from './contexts/OrderContext'
-import { BusinessContext, BusinessProvider, useBusiness } from './contexts/BusinessContext'
-import { SessionContext, SessionProvider, useSession } from './contexts/SessionContext'
-import { WebsocketContext, WebsocketProvider, useWebsocket } from './contexts/WebsocketContext'
-import { ApiContext, ApiProvider, useApi } from './contexts/ApiContext'
-import { EventContext, EventProvider, useEvent } from './contexts/EventContext'
-import { UtilsContext, UtilsProviders, useUtils } from './contexts/UtilsContext'
-import { OrderingContext, OrderingProvider } from './contexts/OrderingContext'
-import { CustomerContext, CustomerProvider, useCustomer } from './contexts/CustomerContext'
-import { ValidationFieldsContext, ValidationFieldsProvider, useValidationFields } from './contexts/ValidationsFieldsContext'
-import { PaymentOptionCash } from './components/PaymentOptionCash'
-import { PaymentOptionPaypal } from './components/PaymentOptionPaypal'
-import { PaymentOptionStripe } from './components/PaymentOptionStripe'
-import { PaymentOptionStripeDirect } from './components/PaymentOptionStripeDirect'
-import { PaymentOptionStripeRedirect } from './components/PaymentOptionStripeRedirect'
-import { StripeElementsForm } from './components/StripeElementsForm'
-import { CardForm } from './components/StripeElementsForm/CardForm'
-import { StripeRedirectForm } from './components/PaymentOptionStripeRedirect/StripeRedirectForm'
-import { FloatingButton } from './components/FloatingButton'
-import { ResetPassword } from './components/ResetPassword'
-import { Analytics } from './components/Analitycs'
-import { AppleLogin } from './components/AppleLogin'
-import { ReCaptcha } from './components/ReCaptcha'
-import { CODES } from './constants/code-numbers'
+import { AddressDetails } from './components/AddressDetails';
+import { AddressForm } from './components/AddressForm';
+import { AddressList } from './components/AddressList';
+import { BaseComponent } from './components/BaseComponent';
+import { BusinessAndProductList } from './components/BusinessAndProductList';
+import { BusinessBasicInformation } from './components/BusinessBasicInformation';
+import { BusinessController } from './components/BusinessController';
+import { BusinessList } from './components/BusinessList';
+import { BusinessInformation } from './components/BusinessInformation';
+import { BusinessProductsCategories } from './components/BusinessProductsCategories';
+import { BusinessProductsSearch } from './components/BusinessProductsSearch';
+import { BusinessReviews } from './components/BusinessReviews';
+import { BusinessSortControl } from './components/BusinessSortControl';
+import { BusinessTypeFilter } from './components/BusinessTypeFilter';
+import { Cart } from './components/Cart';
+import { Checkout } from './components/Checkout';
+import { CouponControl } from './components/CouponControl';
+import { DragAndDrop } from './components/DragAndDrop';
+import { DriverTips } from './components/DriverTips';
+import { ExamineClick } from './components/ExamineClick';
+import { FacebookLoginButton } from './components/FacebookLoginButton';
+import { ForgotPasswordForm } from './components/ForgotPasswordForm';
+import { GoogleLoginButton } from './components/GoogleLoginButton';
+import { GoogleAutocompleteInput } from './components/GoogleAutocompleteInput';
+import { GoogleMaps, GoogleMapsMap } from './components/GoogleMaps';
+import { GpsButton, GoogleGpsButton } from './components/GpsButton';
+import { LanguageSelector } from './components/LanguageSelector';
+import { LoginForm } from './components/LoginForm';
+import { LogoutAction } from './components/LogoutAction';
+import { MainSearch } from './components/MainSearch';
+import { MenuControl } from './components/MenuControl';
+import { MomentOption } from './components/MomentOption';
+import { MyOrders } from './components/MyOrders';
+import { MyOrdersList } from './components/MyOrdersList';
+import { OrderList } from './components/OrderList';
+import { OrderTypeControl } from './components/OrderTypeControl';
+import { PaymentOptions } from './components/PaymentOptions';
+import { Popup } from './components/Popup';
+import { ProductComponent } from './components/ProductComponent';
+import { ProductForm } from './components/ProductForm';
+import { ProductShare } from './components/ProductShare';
+import { ProductImages } from './components/ProductImages';
+import { ProductIngredient } from './components/ProductIngredient';
+import { ProductOption } from './components/ProductOption';
+import { ProductOptionSuboption } from './components/ProductOptionSuboption';
+import { ProductsList } from './components/ProductsList';
+import { ProductsListing } from './components/ProductsListing';
+import { Messages } from './components/Messages';
+import { SearchOptions } from './components/SearchOptions';
+import { SearchOrders } from './components/SearchOrders';
+import { SignupForm } from './components/SignupForm';
+import { SingleBusinessCard } from './components/SingleBusinessCard';
+import { SingleOrderCard } from './components/SingleOrderCard';
+import { SingleProductCard } from './components/SingleProductCard';
+import { UserFormDetails } from './components/UserFormDetails';
+import { UpsellingPage } from './components/UpsellingPage';
+import { WrapperGoogleMaps } from './components/WrapperGoogleMaps';
+import { OrderDetails } from './components/OrderDetails';
+import { OrderReview } from './components/OrderReview';
+import { CmsContent } from './components/CmsContent';
+import { PhoneAutocomplete } from './components/PhoneAutocomplete';
+import { BusinessesMap } from './components/BusinessesMap';
+import {
+  ConfigContext,
+  ConfigProvider,
+  useConfig,
+} from './contexts/ConfigContext';
+import {
+  LanguageContext,
+  LanguageProvider,
+  useLanguage,
+} from './contexts/LanguageContext';
+import { OrderContext, OrderProvider, useOrder } from './contexts/OrderContext';
+import {
+  BusinessContext,
+  BusinessProvider,
+  useBusiness,
+} from './contexts/BusinessContext';
+import {
+  SessionContext,
+  SessionProvider,
+  useSession,
+} from './contexts/SessionContext';
+import {
+  WebsocketContext,
+  WebsocketProvider,
+  useWebsocket,
+} from './contexts/WebsocketContext';
+import { ApiContext, ApiProvider, useApi } from './contexts/ApiContext';
+import { EventContext, EventProvider, useEvent } from './contexts/EventContext';
+import {
+  UtilsContext,
+  UtilsProviders,
+  useUtils,
+} from './contexts/UtilsContext';
+import { OrderingContext, OrderingProvider } from './contexts/OrderingContext';
+import {
+  CustomerContext,
+  CustomerProvider,
+  useCustomer,
+} from './contexts/CustomerContext';
+import {
+  ValidationFieldsContext,
+  ValidationFieldsProvider,
+  useValidationFields,
+} from './contexts/ValidationsFieldsContext';
+import { PaymentOptionCash } from './components/PaymentOptionCash';
+import { PaymentOptionPaypal } from './components/PaymentOptionPaypal';
+import { PaymentOptionStripe } from './components/PaymentOptionStripe';
+import { PaymentOptionStripeDirect } from './components/PaymentOptionStripeDirect';
+import { PaymentOptionStripeRedirect } from './components/PaymentOptionStripeRedirect';
+import { StripeElementsForm } from './components/StripeElementsForm';
+import { CardForm } from './components/StripeElementsForm/CardForm';
+import { StripeRedirectForm } from './components/PaymentOptionStripeRedirect/StripeRedirectForm';
+import { FloatingButton } from './components/FloatingButton';
+import { ResetPassword } from './components/ResetPassword';
+import { Analytics } from './components/Analitycs';
+import { AppleLogin } from './components/AppleLogin';
+import { ReCaptcha } from './components/ReCaptcha';
+import { CODES } from './constants/code-numbers';
 
 export {
   AddressDetails,
@@ -139,6 +172,7 @@ export {
   ProductsList,
   ProductsListing,
   SearchOptions,
+  SearchOrders,
   SignupForm,
   SingleBusinessCard,
   SingleOrderCard,
@@ -200,5 +234,5 @@ export {
   CustomerProvider,
   useCustomer,
   // Constants
-  CODES
-}
+  CODES,
+};
