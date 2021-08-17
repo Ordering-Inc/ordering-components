523ddf67d2a48fb4dbb186911f819ebfd206fde4
Yorman Rodriguez <yorman2414@gmail.com>
2021-08-17 19:57:13 -0400

    Feat: added prop to disable toast in the order context

--- src/contexts/OrderContext/index.js
+++ src/contexts/OrderContext/index.js
@@ -23,7 +23,7 @@ export const OrderContext = createContext()
  * This provider has a reducer for manage order state
  * @param {props} props
  */
-export const OrderProvider = ({ Alert, children, strategy, isAlsea }) => {
+export const OrderProvider = ({ Alert, children, strategy, isAlsea, isDisableToast }) => {
   const [confirmAlert, setConfirm] = useState({ show: false })
   const [alert, setAlert] = useState({ show: false })
   const [ordering] = useApi()
@@ -705,7 +705,10 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea }) => {
    */
   useEffect(() => {
     const handleCartUpdate = (cart) => {
-      showToast(ToastType.Info, t('UPDATING_CART_INFO', 'Updating cart information...'))
+      if (!isDisableToast) {
+        showToast(ToastType.Info, t('UPDATING_CART_INFO', 'Updating cart information...'))
+      }
+
       if (cart.status === 1) {
         if (state.carts[`businessId:${cart.business_id}`]) {
           delete state.carts[`businessId:${cart.business_id}`]
