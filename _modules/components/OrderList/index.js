cf6be9646252b5363605735038d69002e3c5a128
Jesus Perez <jesusdpch00@gmail.com>
2021-09-27 13:09:06 -0400

    build

--- _modules/components/OrderList/index.js
+++ _modules/components/OrderList/index.js
@@ -21,6 +21,8 @@ var _ToastContext = require("../../contexts/ToastContext");
 
 var _LanguageContext = require("../../contexts/LanguageContext");
 
+var _dayjs = _interopRequireDefault(require("dayjs"));
+
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
@@ -591,10 +593,10 @@ var OrderList = function OrderList(props) {
     if (!orderList.loading) {
       var ordersSorted = orderList.orders.sort(function (a, b) {
         if (activeOrders) {
-          return new Date(b.created_at) - new Date(a.created_at);
+          return (0, _dayjs.default)(b.created_at).unix() - (0, _dayjs.default)(a.created_at).unix();
         }
 
-        return new Date(a.created_at) - new Date(b.created_at);
+        return (0, _dayjs.default)(a.created_at).unix() - (0, _dayjs.default)(b.created_at).unix();
       });
       setOrderList(_objectSpread(_objectSpread({}, orderList), {}, {
         orders: ordersSorted
