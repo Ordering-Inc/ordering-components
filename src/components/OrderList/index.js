f451b3d6310c0f54481f74615dd49235013fdf91
carlosg-ordering <68757729+carlosg-ordering@users.noreply.github.com>
2021-12-02 15:42:22 -0600

    [FIX] socket for my order list (#562)
    * [FIX] socket for my order list
    
    * missing change
    
    * missing change x2

--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -250,10 +250,10 @@ export const OrderList = props => {
   useEffect(() => {
     if (!session.user) return
     socket.on('disconnect', (reason) => {
-      const ordersRoom = session?.user?.level === 0 ? 'orders' : `orders_${session?.user?.id}`
+      const ordersRoom = !props.isAsCustomer && session?.user?.level === 0 ? 'orders' : `orders_${session?.user?.id}`
       socket.join(ordersRoom)
     })
-    const ordersRoom = session?.user?.level === 0 ? 'orders' : `orders_${userCustomerId || session?.user?.id}`
+    const ordersRoom = !props.isAsCustomer && session?.user?.level === 0 ? 'orders' : `orders_${session?.user?.id}`
     socket.join(ordersRoom)
     return () => {
       socket.leave(ordersRoom)
@@ -455,5 +455,6 @@ OrderList.defaultProps = {
   beforeComponents: [],
   afterComponents: [],
   beforeElements: [],
-  afterElements: []
+  afterElements: [],
+  isAsCustomer: false
 }
