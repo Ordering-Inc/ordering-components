38a12429e82b1d90ce06def54757c548b063c0cf
carlosq-ordering <carlos.q@ordering.co>
2021-12-28 16:44:57 -0400

    build

--- _modules/index.js
+++ _modules/index.js
@@ -549,6 +549,12 @@ Object.defineProperty(exports, "ResetPassword", {
     return _ResetPassword.ResetPassword;
   }
 });
+Object.defineProperty(exports, "ReviewCustomer", {
+  enumerable: true,
+  get: function get() {
+    return _ReviewCustomer.ReviewCustomer;
+  }
+});
 Object.defineProperty(exports, "ReviewDriver", {
   enumerable: true,
   get: function get() {
@@ -949,6 +955,8 @@ var _Analitycs = require("./components/Analitycs");
 var _AppleLogin = require("./components/AppleLogin");
 
 var _ReCaptcha = require("./components/ReCaptcha");
+
+var _ReviewCustomer = require("./components/ReviewCustomer");
 
 var _ReviewProduct = require("./components/ReviewProduct");
 
