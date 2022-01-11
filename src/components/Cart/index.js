f8b9e1ffce06322cdd6eee33d4776565dc2ccdf7
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-01-11 17:18:32 -0400

    [ADD] cart comments (#591)
    * [ADD] cart comments
    
    * [ADD] comment functions
    
    * [ADD] comment functions

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { useOrder } from '../../contexts/OrderContext'
 import { useConfig } from '../../contexts/ConfigContext'
+import { useApi } from '../../contexts/ApiContext'
+import { useSession } from '../../contexts/SessionContext'
+import { useToast, ToastType } from '../../contexts/ToastContext'
 
 export const Cart = (props) => {
   const {
@@ -21,6 +24,26 @@ export const Cart = (props) => {
   const [stateConfig] = useConfig()
 
   /**
+   * Api context
+   */
+  const [ordering] = useApi()
+
+  /**
+   * Session content
+   */
+  const [{ token }] = useSession()
+
+  /**
+   * Comment state
+   */
+  const [commentState, setCommentState] = useState({ loading: false, result: null, error: null })
+
+  /**
+   * Toast state
+   */
+  const [, { showToast }] = useToast()
+
+  /**
    * Total product in cart
    */
   const totalBalance = stateConfig.order?.quantity || 0
@@ -29,6 +52,14 @@ export const Cart = (props) => {
    * Max total product in cart by config
    */
   const maxCartProductConfig = (stateConfig.configs.max_product_amount ? parseInt(stateConfig.configs.max_product_amount) : 100) - totalBalance
+  /**
+   * Timeout for update cart comment
+   */
+  let timeout = null
+  /**
+   * Cart comment stagged
+   */
+  let previousComment
   /**
    * Calc balance by product id
    */
@@ -62,6 +93,43 @@ export const Cart = (props) => {
     }
   }
 
+  /**
+   * change comment for cart
+   */
+  const handleChangeComment = (value) => {
+    try {
+      if (previousComment !== value) {
+        clearTimeout(timeout)
+        timeout = setTimeout(async function () {
+          setCommentState({ ...commentState, loading: true })
+          const uuid = cart?.uuid
+          const response = await fetch(`${ordering.root}/carts/${uuid}`, {
+            'Content-Type': 'application/json',
+            method: 'PUT',
+            body: JSON.stringify({
+              comment: value
+            }),
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`
+            }
+          })
+          const { result, error } = await response.json()
+          if (error) {
+            setCommentState({ ...commentState, loading: false, error: true, result })
+            showToast(ToastType.Error, result)
+            return
+          }
+          setCommentState({ ...commentState, loading: false, error: null, result })
+        }, 750)
+      }
+      previousComment = value
+    } catch (err) {
+      setCommentState({ ...commentState, loading: false, error: true, result: err.message })
+      showToast(ToastType.Error, err.message)
+    }
+  }
+
   return (
     <>
       {UIComponent && (
@@ -75,6 +143,8 @@ export const Cart = (props) => {
           getProductMax={getProductMax}
           offsetDisabled={offsetDisabled}
           handleEditProduct={handleEditProduct}
+          handleChangeComment={handleChangeComment}
+          commentState={commentState}
         />
       )}
     </>
