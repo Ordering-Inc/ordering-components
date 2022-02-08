580a8e3d1e1aaafd0c0726fa7b32af994f05990b
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-02-08 16:21:18 -0400

    Smart banner (#619)
    * [ADD] smart banner
    
    * build
    
    * clean build
    
    * [ADD] optimize metas effect
    
    * [ADD] filter for metas without id

---
+++ src/components/SmartAppBanner/index.js
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react'
+import { useConfig } from '../../contexts/ConfigContext'
+import PropTypes from 'prop-types'
+
+export const SmartAppBanner = (props) => {
+  const {
+    UIComponent,
+    storeAndroidId,
+    storeAppleId,
+    storeKindleId
+  } = props
+
+  const [{ configs }] = useConfig()
+
+  useEffect(() => {
+    if (!configs) return
+    const metas = [
+      {
+        google: {
+          name: 'google-play-app',
+          id: storeAndroidId || configs?.android_app_id?.value
+        },
+        apple: {
+          name: 'apple-itunes-app',
+          id: storeAppleId || configs?.ios_app_id?.value
+        },
+        kindle: {
+          name: 'kindle-fire-app',
+          id: storeKindleId || configs?.kindle_app_id?.value
+        }
+      }
+    ]
+    metas.filter(meta => meta?.id).map(meta => {
+      const metaElement = document.createElement('meta')
+      metaElement.name = meta.name
+      metaElement.content = `app-id=${meta.id}`
+      document.getElementsByTagName('head')[0].appendChild(metaElement)
+    })
+  }, [configs])
+
+  return (
+    <>
+      {UIComponent && (
+        <UIComponent {...props} />
+      )}
+    </>
+  )
+}
+
+SmartAppBanner.propTypes = {
+  /**
+   * UI Component, this must be containt all graphic elements and use parent props
+   */
+  UIComponent: PropTypes.elementType,
+  /**
+   * store android id
+   */
+  storeAndroidId: PropTypes.string,
+  /**
+   * store apple id
+   */
+  storeAppleId: PropTypes.string,
+  /**
+   * store kindle id
+   */
+  storeKindleId: PropTypes.string
+}
+
+SmartAppBanner.defaultProps = {
+  storeAndroidId: '',
+  storeAppleId: '',
+  storeKindleId: ''
+}
