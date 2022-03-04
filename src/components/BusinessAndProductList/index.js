a55fb07388baf5e3bf612d4adf3552b8f56d255b
Jesus David <jesusdpch00@gmail.com>
2022-03-04 17:37:01 -0400

    [FIX] order of rank products

--- src/components/BusinessAndProductList/index.js
+++ src/components/BusinessAndProductList/index.js
@@ -129,7 +129,7 @@ export const BusinessAndProductList = (props) => {
 
   const sortProductsArray = (option, array) => {
     if (option === 'rank' || option === null) {
-      return array.sort((a, b) => b.rank - a.rank)
+      return array.sort((a, b) => a.rank - b.rank)
     }
     if (option === 'a-z') {
       return array.sort((a, b) =>
