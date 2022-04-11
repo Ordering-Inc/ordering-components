c909e820375816ce9d88fb159697b061b0c1e6d4
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-04-11 14:44:56 -0400

    export business search list (#668)

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import { BusinessProductsSearch } from './components/BusinessProductsSearch'
 import { BusinessReviews } from './components/BusinessReviews'
 import { BusinessSortControl } from './components/BusinessSortControl'
 import { BusinessTypeFilter } from './components/BusinessTypeFilter'
+import { BusinessSearchList } from './components/BusinessSearchList'
 import { Cart } from './components/Cart'
 import { CartStoresListing } from './components/CartStoresListing'
 import { Checkout } from './components/Checkout'
@@ -125,6 +126,7 @@ export {
   BusinessReviews,
   BusinessSortControl,
   BusinessTypeFilter,
+  BusinessSearchList,
   Cart,
   CartStoresListing,
   Checkout,
