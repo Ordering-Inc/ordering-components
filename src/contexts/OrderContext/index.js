797c6ba854a13b1d3808e165eecf543a1855fb84
carlosg-ordering <68757729+carlosg-ordering@users.noreply.github.com>
2022-04-22 09:20:04 -0500

    [FIX] reload when cart update (#675)

--- src/contexts/OrderContext/index.js
+++ src/contexts/OrderContext/index.js
@@ -51,7 +51,8 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea, isDisableToa
       ? { type: null, moment: null }
       : {
         type: orderTypes[configState?.configs?.default_order_type?.value],
-        moment: null
+        moment: null,
+        address: { location : { lat: null , lng: null } }
       },
     carts: {},
     confirmAlert,
