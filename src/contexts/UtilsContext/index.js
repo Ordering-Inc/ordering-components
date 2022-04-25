cbc3bf244b50fa2958060426791ad39a2f3041e2
Jesus David <jesusdpch00@gmail.com>
2022-04-25 17:35:34 -0400

    return url only

--- src/contexts/UtilsContext/index.js
+++ src/contexts/UtilsContext/index.js
@@ -252,11 +252,6 @@ export const UtilsProviders = ({ children }) => {
 
   const optimizeImage = (url, params, fallback) => {
     if (!url && fallback) return fallback
-    params = params && params.length > 0 ? `,${params}` : ''
-    if (url != null && url.indexOf('res.cloudinary.com') !== -1) {
-      var parts = url.split('upload')
-      url = `${parts[0]}upload${parts[1]}`
-    }
     return url
   }
 
