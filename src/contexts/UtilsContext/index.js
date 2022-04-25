42a938591627266c1acf4018590e74669034b8e1
Jesus David <jesusdpch00@gmail.com>
2022-04-25 16:29:50 -0400

    [FIX] optimize image doble transformation

--- src/contexts/UtilsContext/index.js
+++ src/contexts/UtilsContext/index.js
@@ -255,7 +255,7 @@ export const UtilsProviders = ({ children }) => {
     params = params && params.length > 0 ? `,${params}` : ''
     if (url != null && url.indexOf('res.cloudinary.com') !== -1) {
       var parts = url.split('upload')
-      url = `${parts[0]}upload/f_auto,q_auto${params}${parts[1]}`
+      url = `${parts[0]}upload/${parts[1]}`
     }
     return url
   }
