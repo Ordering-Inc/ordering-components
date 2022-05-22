cf4e1826a3d67d43da1477293503096e6fda976e
Jesus David <jesusdpch00@gmail.com>
2022-05-22 17:51:50 -0400

    [remove] unused code

--- src/components/PromotionsController/index.js
+++ src/components/PromotionsController/index.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useApi } from '../../contexts/ApiContext'
 import { useSession } from '../../contexts/SessionContext'
 import { useOrder } from '../../contexts/OrderContext'
+
 export const PromotionsController = (props) => {
   const {
     UIComponent,
-    paramsToFetch,
-    paginationSettings
+    paramsToFetch
   } = props
 
   const [session] = useSession()
@@ -18,17 +18,10 @@ export const PromotionsController = (props) => {
   const [offersState, setOffersState] = useState({
     loading: true,
     error: null,
-    offers: [],
-    pagination: {
-      currentPage: (paginationSettings.controlType === 'pages' && paginationSettings.initialPage && paginationSettings.initialPage >= 1)
-        ? paginationSettings.initialPage - 1
-        : 0,
-      pageSize: paginationSettings.pageSize ?? 10,
-      total: null
-    }
+    offers: []
   })
 
-  const getOffers = async ({ page, pageSize = paginationSettings.pageSize }) => {
+  const getOffers = async () => {
     const location = JSON.stringify(options?.address?.location)
     const url = `${ordering.root}/offers/public?enabled=true&params=${paramsToFetch.join()}&location=${location}`
 
@@ -43,64 +36,14 @@ export const PromotionsController = (props) => {
   }
 
   const loadOffers = async () => {
-    if (
-      (offersState.pagination?.currentPage === offersState.pagination?.totalPages &&
-        offersState.pagination?.total !== null) && !searchValue
-    ) {
-      return
-    }
-
     try {
       setOffersState({ ...offersState, loading: true })
-      const { error, result, pagination } = await getOffers({ page: 1 })
+      const { error, result } = await getOffers()
       setOffersState({
         ...offersState,
         loading: false,
         error: error ? result : null,
-        offers: error ? [] : result,
-        pagination: !error
-          ? {
-            ...offersState.pagination,
-            currentPage: pagination?.current_page,
-            pageSize: pagination?.page_size,
-            totalPages: pagination?.total_pages,
-            total: pagination?.total,
-            from: pagination?.from,
-            to: pagination?.to
-          }
-          : offersState.pagination
-      })
-    } catch (err) {
-      setOffersState({
-        ...offersState,
-        loading: false,
-        error: [err?.message ?? 'ERROR']
-      })
-    }
-  }
-
-  const loadMoreOffers = async () => {
-    try {
-      setOffersState({ ...offersState, loading: true })
-      const { error, result, pagination } = await getOffers({
-        page: offersState.pagination?.currentPage + 1
-      })
-      setOffersState({
-        ...offersState,
-        loading: false,
-        error: error ? result : null,
-        offers: error ? offersState.offers : offersState.offers.concat(result),
-        pagination: !error
-          ? {
-            ...offersState.pagination,
-            currentPage: pagination.current_page,
-            pageSize: pagination.page_size,
-            totalPages: pagination.total_pages,
-            total: pagination.total,
-            from: pagination.from,
-            to: pagination.to
-          }
-          : offersState.pagination
+        offers: error ? [] : result
       })
     } catch (err) {
       setOffersState({
@@ -127,7 +70,6 @@ export const PromotionsController = (props) => {
           offersState={offersState}
           searchValue={searchValue}
           offerSelected={offerSelected}
-          loadMoreOffers={loadMoreOffers}
           handleSearchValue={handleSearchValue}
           setOfferSelected={setOfferSelected}
         />
@@ -168,10 +110,5 @@ PromotionsController.defaultProps = {
     'valid_from_after_user_last_order_minutes',
     'valid_until_after_user_last_order_minutes',
     'include_products_with_offer'
-  ],
-  paginationSettings: {
-    initialPage: 1,
-    pageSize: 10,
-    controlType: 'infinity'
-  }
+  ]
 }
