ed2eb6992f758ad5e7fc0ba75ed8d9fb38fef0e6
Min <skyandarti@outlook.com>
2022-06-23 15:00:17 +0800

    Update: build

--- native/index.js
+++ native/index.js
@@ -3,6 +3,7 @@ import { AddressForm } from '../src/components/AddressForm'
 import { AddressList } from '../src/components/AddressList'
 import { BusinessAndProductList } from '../src/components/BusinessAndProductList'
 import { BusinessBasicInformation } from '../src/components/BusinessBasicInformation'
+import { BusinessBrandFilter } from '../src/components/BusinessBrandFilter'
 import { BusinessController } from '../src/components/BusinessController'
 import { BusinessList } from '../src/components/BusinessList'
 import { BusinessInformation } from '../src/components/BusinessInformation'
@@ -100,6 +101,7 @@ export {
   AddressList,
   BusinessAndProductList,
   BusinessBasicInformation,
+  BusinessBrandFilter,
   BusinessController,
   BusinessList,
   BusinessInformation,
