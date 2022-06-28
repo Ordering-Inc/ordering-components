a9e2616f40bca274c94ad25685ca255036828403
Carlos Quiroz <carloseqv29596@gmail.com>
2022-06-28 10:52:15 -0400

    build

--- _modules/components/PromotionsController/index.js
+++ _modules/components/PromotionsController/index.js
@@ -180,6 +180,7 @@ var PromotionsController = function PromotionsController(props) {
     offersState: offersState,
     searchValue: searchValue,
     offerSelected: offerSelected,
+    loadOffers: loadOffers,
     handleSearchValue: handleSearchValue,
     setOfferSelected: setOfferSelected
   })));
