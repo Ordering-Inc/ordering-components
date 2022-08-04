8923d1bed2605fabf78fc84a2cea0572d727d567
Jesus David <jesusdpch00@gmail.com>
2022-08-04 18:41:28 -0400

    export theme context

--- src/index.js
+++ src/index.js
@@ -93,6 +93,7 @@ import { OrderingContext, OrderingProvider } from './contexts/OrderingContext'
 import { CustomerContext, CustomerProvider, useCustomer } from './contexts/CustomerContext'
 import { ValidationFieldsContext, ValidationFieldsProvider, useValidationFields } from './contexts/ValidationsFieldsContext'
 import { ToastContext, ToastProvider, useToast, ToastType } from './contexts/ToastContext'
+import { OrderingThemeContext, OrderingThemeProvider, useOrderingTheme } from './contexts/OrderingThemeContext'
 import { PlaceSpot } from './components/PlaceSpot'
 import { PaymethodList } from './components/PaymethodList'
 import { PaymentOptionCash } from './components/PaymentOptionCash'
@@ -262,6 +263,9 @@ export {
   ToastProvider,
   useToast,
   ToastType,
+  OrderingThemeContext,
+  OrderingThemeProvider,
+  useOrderingTheme,
   // Constants
   CODES
 }
