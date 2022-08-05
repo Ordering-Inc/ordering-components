9123aaae361f9d61b63395f470fb529e92e8b323
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-08-06 06:31:02 +0800

    [Feat] Site config (#765)
    * build
    
    * import site context
    
    * build
    
    * [Fix] bug
    
    * build
    
    * [update] site endpoint
    
    * build
    
    * fix bug
    
    * build
    
    * delete build files

---
+++ src/contexts/SiteContext/index.js
@@ -0,0 +1,68 @@
+import React, { createContext, useState, useContext, useEffect } from 'react'
+import { useApi } from '../ApiContext'
+import { useLanguage } from '../LanguageContext'
+
+/**
+ * Create SiteContext
+ * This context will manage the current site internally and provide an easy interface
+ */
+export const SiteContext = createContext()
+
+/**
+ * Custom provider to site manager
+ * This provider has a reducer for manage site state
+ * @param {props} props
+ */
+export const SiteProvider = ({ appId, children }) => {
+  const [state, setState] = useState({ loading: true, site: {} })
+  const [languageState] = useLanguage()
+  const [ordering] = useApi()
+
+  const refreshSite = async () => {
+    try {
+      setState({ ...state, loading: true })
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-APP-X': appId
+        }
+      }
+
+      const response = await fetch(`${ordering.root}/sites/current`, requestOptions)
+      const { error, result } = await response.json()
+
+      setState({
+        ...state,
+        loading: false,
+        site: error ? {} : result
+      })
+    } catch (err) {
+      setState({ ...state, loading: false })
+    }
+  }
+
+  const functions = {
+    refreshSite
+  }
+
+  useEffect(() => {
+    if (!languageState.loading) {
+      refreshSite()
+    }
+  }, [languageState])
+
+  return (
+    <SiteContext.Provider value={[state, functions]}>
+      {children}
+    </SiteContext.Provider>
+  )
+}
+
+/**
+ * Hook to get and update site state
+ */
+export const useSite = () => {
+  const siteManager = useContext(SiteContext)
+  return siteManager || [{}, async () => {}]
+}
