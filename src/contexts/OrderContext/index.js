d6118c6477f03ad826b5677f4a4d0d03ba946218
Jesus David <jesusdpch00@gmail.com>
2022-08-10 20:42:51 -0400

    [ADD] city id on local storage

--- src/contexts/OrderContext/index.js
+++ src/contexts/OrderContext/index.js
@@ -830,7 +830,8 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea, isDisableToa
         : {
           type: optionsLocalStorage?.type || orderTypes[configState?.configs?.default_order_type?.value],
           moment: optionsLocalStorage?.moment || null,
-          address: optionsLocalStorage?.address || state?.options?.address || {}
+          address: optionsLocalStorage?.address || state?.options?.address || {},
+          city_id: optionsLocalStorage?.city_id || null
         }
     })
   }
