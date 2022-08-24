0b4c431ad79167f254fcb4c0c1b201031754a298
Carlos Quiroz <carloseqv29596@gmail.com>
2022-08-23 22:34:27 -0400

    [FEAT] add billing context

---
+++ src/contexts/BillingContext/index.js
@@ -0,0 +1,39 @@
+import React, { createContext } from 'react'
+import { ConfigProvider } from '../ConfigContext'
+import { SessionProvider } from '../SessionContext'
+import { LanguageProvider } from '../LanguageContext'
+import { ApiProvider } from '../ApiContext'
+import { EventProvider } from '../EventContext'
+import { ToastProvider } from '../ToastContext'
+import { WebStrategy } from '../../webStrategy'
+/**
+ * Create BillingContext
+ * Wrapper to use all context to ordering apps
+ */
+export const BillingContext = createContext()
+
+/**
+ * Custom provider to languages manager
+ * This provider has a reducer for manage languages state
+ * @param {props} props
+ */
+export const BillingProvider = ({ settings, children }) => {
+  const webStrategy = new WebStrategy()
+  return (
+    <BillingContext.Provider>
+      <EventProvider>
+        <ApiProvider settings={Object.assign(settings.api, { project: settings.project, appId: settings.app_id })}>
+          <LanguageProvider strategy={webStrategy}>
+            <ConfigProvider strategy={webStrategy}>
+              <ToastProvider>
+                <SessionProvider strategy={webStrategy}>
+                  {children}
+                </SessionProvider>
+              </ToastProvider>
+            </ConfigProvider>
+          </LanguageProvider>
+        </ApiProvider>
+      </EventProvider>
+    </BillingContext.Provider>
+  )
+}
