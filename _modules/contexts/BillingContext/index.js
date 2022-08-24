dee108afa6cab52532ec799967c7ff08ae15da58
Carlos Quiroz <carloseqv29596@gmail.com>
2022-08-23 22:51:51 -0400

    build

--- _modules/contexts/BillingContext/index.js
+++ _modules/contexts/BillingContext/index.js
@@ -45,10 +45,7 @@ var BillingProvider = function BillingProvider(_ref) {
       children = _ref.children;
   var webStrategy = new _webStrategy.WebStrategy();
   return /*#__PURE__*/_react.default.createElement(BillingContext.Provider, null, /*#__PURE__*/_react.default.createElement(_EventContext.EventProvider, null, /*#__PURE__*/_react.default.createElement(_ApiContext.ApiProvider, {
-    settings: Object.assign(settings.api, {
-      project: settings.project,
-      appId: settings.app_id
-    })
+    settings: settings
   }, /*#__PURE__*/_react.default.createElement(_LanguageContext.LanguageProvider, {
     strategy: webStrategy
   }, /*#__PURE__*/_react.default.createElement(_ConfigContext.ConfigProvider, {
