50c17cbd8a9e1811e280997c0776b51046c9ea5a
MingXue <mingxue@ordering.co>
2022-09-13 01:03:17 +0800

    Feedback recaptchav3 (#792)
    * add recaptcha v3 package
    
    * update recaptcha
    
    * add recaptcha v3
    
    * remove unused code

--- src/components/ReCaptcha/index.js
+++ src/components/ReCaptcha/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReCAPTCHA from 'react-google-recaptcha'
+import { GoogleReCaptchaProvider, GoogleReCaptcha } from 'react-google-recaptcha-v3'
 import { useConfig } from '../../contexts/ConfigContext'
 import PropTypes from 'prop-types'
 
@@ -9,6 +10,7 @@ export const ReCaptcha = (props) => {
   } = props
 
   const [{ configs }] = useConfig()
+  const [reCaptchaVersion, setRecaptchaVersion] = useState({ version: 'v2', siteKey: '' })
 
   /**
    * Change reCaptcha
@@ -17,18 +19,37 @@ export const ReCaptcha = (props) => {
     handleReCaptcha(value)
   }
 
-  const getReCaptChaSiteKey = () => {
-    if (configs && Object.keys(configs).length > 0 && configs?.security_recaptcha_site_key?.value) {
-      return configs?.security_recaptcha_site_key.value
+  useEffect(() => {
+    if (configs && Object.keys(configs).length > 0 &&
+      configs?.security_recaptcha_type?.value === 'v3' &&
+      configs?.security_recaptcha_score_v3?.value > 0 &&
+      configs?.security_recaptcha_site_key_v3?.value
+    ) {
+      setRecaptchaVersion({ version: 'v3', siteKey: configs?.security_recaptcha_site_key_v3?.value })
+      return
     }
-    throw new Error('ReCaptcha component: the config doesn\'t have recaptcha site key')
-  }
+    if (configs && Object.keys(configs).length > 0 && configs?.security_recaptcha_site_key?.value) {
+      setRecaptchaVersion({ version: 'v2', siteKey: configs?.security_recaptcha_site_key?.value })
+      return
+    }
+    if (configs && Object.keys(configs).length > 0) {
+      throw new Error('ReCaptcha component: the config doesn\'t have recaptcha site key')
+    }
+  }, [configs])
 
   return (
-    <ReCAPTCHA
-      sitekey={getReCaptChaSiteKey()}
-      onChange={onChange}
-    />
+    <>
+      {reCaptchaVersion?.version === 'v2' ? (
+        <ReCAPTCHA
+          sitekey={reCaptchaVersion?.siteKey}
+          onChange={onChange}
+        />
+      ) : (
+        <GoogleReCaptchaProvider reCaptchaKey={reCaptchaVersion?.siteKey}>
+          <GoogleReCaptcha onVerify={onChange} />
+        </GoogleReCaptchaProvider>
+      )}
+    </>
   )
 }
 
