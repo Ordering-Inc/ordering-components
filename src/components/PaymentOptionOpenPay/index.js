16f9f3532261f4488ab7cc9d6a2116b3958c4daa
Jesus David <jesusdpch00@gmail.com>
2022-09-16 17:07:03 -0400

    [add] openpay and wow rewards

---
+++ src/components/PaymentOptionOpenPay/index.js
@@ -0,0 +1,274 @@
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { useSession } from '../../contexts/SessionContext'
+import { useApi } from '../../contexts/ApiContext'
+
+/**
+ * Component to manage payment option paypal behavior without UI component
+ */
+export const PaymentOptionOpenPay = (props) => {
+  const {
+    UIComponent,
+    publicKey,
+    merchantId,
+    isSandbox
+  } = props
+
+  const [{ token, user }] = useSession()
+  const [cardsList, setCardsList] = useState({ cards: [], loading: true, error: null })
+  const [isSdkReady, setIsSdkReady] = useState(false)
+  const [ordering] = useApi()
+  const [cardSelected, setCardSelected] = useState(null)
+
+  const isAlsea = ordering.project === 'alsea'
+
+  useEffect(() => {
+    if (!merchantId || !publicKey) return
+    if (window?.OpenPay) {
+      setIsSdkReady(true)
+      return
+    }
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = 'https://resources.openpay.mx/lib/openpay-js/1.2.38/openpay.v1.min.js'
+    script.async = true
+    script.onload = () => {
+      setIsSdkReady(true)
+      window.OpenPay.setId(merchantId)
+      window.OpenPay.setApiKey(publicKey)
+      window.OpenPay.setSandboxMode(isSandbox)
+    }
+    script.onerror = () => {
+      throw new Error('Open pay SDK could not be loaded.')
+    }
+
+    document.body.appendChild(script)
+    return () => {
+      script.onload = null
+    }
+  }, [merchantId, publicKey])
+
+  const getCards = async () => {
+    try {
+      setCardsList({
+        ...cardsList,
+        loading: true,
+        error: null
+      })
+      const response = await fetch(`https://alsea-plugins${isAlsea ? '' : '-staging'}.ordering.co/alseaplatform/api/openpay/cards/cards.php?language=${ordering.language}&user_id=${user?.id}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'X-APP-X': ordering.appId
+        }
+      })
+      const result = await response.json()
+      if (result.error) {
+        setCardsList({
+          loading: false,
+          cards: [],
+          error: result?.result
+        })
+      } else {
+        setCardsList({
+          loading: false,
+          cards: result?.result.map(card => ({
+            ...card,
+            data: {
+              card_id: card?.id,
+              device_session_id: window.OpenPay.deviceData.setup()
+            }
+          })),
+          error: null
+        })
+      }
+    } catch (err) {
+      setCardsList({
+        ...cardsList,
+        loading: false,
+        error: [err.message]
+      })
+    }
+  }
+
+  const handleCardClick = (card) => {
+    setCardSelected({
+      id: card.id,
+      type: 'card',
+      card: {
+        brand: card.brandCardName,
+        last4: card.last4
+      },
+      data: {
+        card_id: card.id,
+        device_session_id: window.OpenPay.deviceData.setup()
+      }
+    })
+  }
+
+  const handleNewCard = async (data) => {
+    try {
+      setCardsList({
+        ...cardsList,
+        loading: true,
+        error: null
+      })
+      const response = await fetch(`https://alsea-plugins${isAlsea ? '' : '-staging'}.ordering.co/alseaplatform/api/openpay/cards/token.php`, {
+        method: 'POST',
+        body: JSON.stringify({
+          card_number: data.cardNumber,
+          cvv2: data.cardSecurityCode,
+          // expiry: data.expiry,
+          expiration_month: data.cardMonth,
+          expiration_year: data.cardYear,
+          holder_name: data.cardName
+        }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'X-APP-X': ordering.appId
+        }
+      })
+      const result = await response.json()
+      if (typeof result !== 'object' || result?.error) {
+        setCardsList({
+          ...cardsList,
+          loading: false,
+          error: result?.result
+        })
+        return
+      }
+      const deviceSessionId = window.OpenPay.deviceData.setup()
+      const responseCard = await fetch(`https://alsea-plugins${isAlsea ? '' : '-staging'}.ordering.co/alseaplatform/api/openpay/cards/add.php`, {
+        method: 'POST',
+        body: JSON.stringify({
+          language: ordering.language,
+          user_id: user?.id,
+          token_id: result?.id,
+          device_session_id: deviceSessionId
+        }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'X-APP-X': ordering.appId,
+          'Content-Type': 'application/json'
+        }
+      })
+      const resultCard = await responseCard.json()
+      if (resultCard.error) {
+        setCardsList({
+          ...cardsList,
+          loading: false,
+          error: resultCard?.result
+        })
+        return
+      }
+      setCardsList({
+        cards: [
+          ...cardsList.cards,
+          {
+            ...resultCard?.result,
+            brand: resultCard?.brandCardName
+          }
+        ],
+        loading: false,
+        error: null
+      })
+      handleCardClick({
+        ...resultCard?.result,
+        brand: resultCard?.brandCardName,
+        type: 'card',
+        card: {
+          brand: resultCard.brandCardName,
+          last4: resultCard?.last4
+        },
+        data: {
+          card_id: resultCard?.result?.id,
+          device_session_id: deviceSessionId
+        }
+      })
+    } catch (err) {
+      setCardsList({
+        ...cardsList,
+        loading: false,
+        error: [err.message]
+      })
+    }
+  }
+
+  const deleteCard = async (card) => {
+    try {
+      setCardsList({
+        ...cardsList,
+        loading: true,
+        error: null
+      })
+      const response = await fetch(`https://alsea-plugins${isAlsea ? '' : '-staging'}.ordering.co/alseaplatform/api/openpay/cards/delete.php`, {
+        method: 'POST',
+        body: JSON.stringify({
+          language: ordering.language,
+          user_id: user?.id,
+          card_id: card?.id
+        }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'X-APP-X': ordering.appId
+        }
+      })
+      const result = await response.json()
+      if (result?.result === 'OK') {
+        setCardsList({
+          cards: cardsList?.cards?.filter(_card => _card?.id !== card?.id),
+          loading: false,
+          error: null
+        })
+      } else {
+        setCardsList({
+          ...cardsList,
+          loading: false,
+          error: [result?.result]
+        })
+      }
+    } catch (err) {
+      setCardsList({
+        ...cardsList,
+        loading: false,
+        error: [err.message]
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (isSdkReady) {
+      getCards()
+    }
+  }, [isSdkReady])
+
+  return (
+    UIComponent && (
+      <UIComponent
+        {...props}
+        cardSelected={cardSelected}
+        getCards={getCards}
+        cardsList={cardsList}
+        handleNewCard={handleNewCard}
+        handleCardClick={handleCardClick}
+        deleteCard={deleteCard}
+      />
+    )
+  )
+}
+
+PaymentOptionOpenPay.propTypes = {
+  /**
+   * UI Component, this must be containt all graphic elements and use parent props
+   */
+  UIComponent: PropTypes.elementType
+}
+
+PaymentOptionOpenPay.defaultProps = {
+  body: {},
+  currency: 'USD',
+  beforeComponents: [],
+  afterComponents: [],
+  beforeElements: [],
+  afterElements: []
+}
--- src/components/PaymentOptions/index.js
+++ src/components/PaymentOptions/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { useOrder } from '../../contexts/OrderContext'
 import { useApi } from '../../contexts/ApiContext'
 import { useEvent } from '../../contexts/EventContext'
-
+import { useSession } from '../../contexts/SessionContext'
 const paymethodsExisting = ['stripe', 'stripe_direct', 'stripe_connect', 'paypal', 'square']
 const paymethodsNotAllowed = ['paypal_express', 'authorize']
 const paymethodsCallcenterMode = ['cash', 'card_delivery', 'ivrpay', '100_coupon']
@@ -25,18 +25,20 @@ export const PaymentOptions = (props) => {
   const [events] = useEvent()
   const [ordering] = useApi()
   const [orderState, { changePaymethod }] = useOrder()
+  const [{ user, token }] = useSession()
   const orderTotal = orderState.carts?.[`businessId:${businessId}`]?.total || 0
+  const isAlsea = ordering.project === 'alsea'
 
   const [paymethodsList, setPaymethodsList] = useState({ paymethods: [], loading: true, error: null })
   const [paymethodSelected, setPaymethodsSelected] = useState(null)
   const [paymethodData, setPaymethodData] = useState({})
   const [isOpenMethod, setIsOpenMethod] = useState({ paymethod: null })
-
+  const [wowPoints, setWowPoints] = useState({ loading: false, points: null, error: null })
   const parsePaymethods = (paymethods) => {
     const _paymethods = paymethods && paymethods
       .filter(credentials => isCustomerMode
         ? !paymethodsNotAllowed.includes(credentials?.paymethod?.gateway) &&
-          paymethodsCallcenterMode.includes(credentials?.paymethod?.gateway)
+        paymethodsCallcenterMode.includes(credentials?.paymethod?.gateway)
         : !paymethodsNotAllowed.includes(credentials?.paymethod?.gateway))
       .map(credentials => {
         return {
@@ -58,6 +60,7 @@ export const PaymentOptions = (props) => {
       if (!error) {
         paymethodsList.paymethods = parsePaymethods(result.paymethods)
       }
+
       setPaymethodsList({
         ...paymethodsList,
         loading: false,
@@ -111,7 +114,7 @@ export const PaymentOptions = (props) => {
         id: paymethod?.id,
         gateway: paymethod?.gateway,
         paymethod: paymethod,
-        credentials : paymethod?.credentials ?? null,
+        credentials: paymethod?.credentials ?? null,
         data
       })
       return
@@ -130,6 +133,50 @@ export const PaymentOptions = (props) => {
     }
   }
 
+  const getWowPoints = async () => {
+    try {
+      setWowPoints({
+        ...wowPoints,
+        loading: true
+      })
+      const response = await fetch(`https://alsea-plugins${isAlsea ? '' : '-staging'}.ordering.co/alseaplatform/wow_check_profile.php`, {
+        method: 'POST',
+        body: JSON.stringify({
+          id: user?.id,
+          mail: user?.email,
+          name: user?.name,
+          lastname: user?.lastname,
+          cellphone: user?.cellphone
+        }),
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'X-APP-X': ordering.appId,
+          'Content-Type': 'application/json'
+        }
+      })
+      const result = await response.json()
+      if (result?.user?.points) {
+        setWowPoints({
+          error: false,
+          loading: false,
+          points: result?.user?.points
+        })
+      } else {
+        setWowPoints({
+          error: true,
+          loading: false,
+          points: '???'
+        })
+      }
+    } catch (err) {
+      setWowPoints({
+        error: true,
+        loading: false,
+        points: '???'
+      })
+    }
+  }
+
   useEffect(() => {
     if (paymethodSelected) {
       changePaymethod(businessId, paymethodSelected.id, JSON.stringify(paymethodData))
@@ -140,7 +187,7 @@ export const PaymentOptions = (props) => {
     if (
       paymethodSelected &&
       (['card_delivery', 'cash', 'stripe_redirect'].includes(paymethodSelected?.gateway) ||
-      !paymethodsExisting.includes(paymethodSelected?.gateway))
+        !paymethodsExisting.includes(paymethodSelected?.gateway))
     ) {
       onPaymentChange && onPaymentChange({
         paymethodId: paymethodSelected.id,
@@ -162,6 +209,9 @@ export const PaymentOptions = (props) => {
         loading: isLoading,
         paymethods: parsePaymethods(paymethods)
       })
+      if (paymethods.find(paymethod => paymethod?.paymethod?.gateway === 'wow_rewards')) {
+        getWowPoints()
+      }
     } else {
       if (businessId) {
         getPaymentOptions()
@@ -179,6 +229,7 @@ export const PaymentOptions = (props) => {
           paymethodsList={paymethodsList}
           paymethodSelected={paymethodSelected}
           paymethodData={paymethodData}
+          wowPoints={wowPoints}
           setPaymethodData={setPaymethodData}
           handlePaymethodClick={handlePaymethodClick}
           handlePaymethodDataChange={handlePaymethodDataChange}
