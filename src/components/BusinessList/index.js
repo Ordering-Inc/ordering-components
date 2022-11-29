037738b8f45b83f73fb51dcebe5c9ac6244fda76
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2022-11-29 15:18:27 -0400

    [FEAT] improve business list component (#837)
    Co-authored-by: Carlos Quiroz <carloseqv29596@gmail.com>

--- src/components/BusinessList/index.js
+++ src/components/BusinessList/index.js
@@ -80,7 +80,6 @@ export const BusinessList = (props) => {
         loading: true,
         businesses: newFetch ? [] : businessesList.businesses
       })
-      refreshConfigs()
       let parameters = asDashboard ? {} : {
         location: !customLocation
           ? `${orderState.options?.address?.location?.lat},${orderState.options?.address?.location?.lng}`
@@ -357,11 +356,26 @@ export const BusinessList = (props) => {
    * Listening order option and filter changes
    */
   useEffect(() => {
-    if ((orderState.loading || (!orderState.options?.address?.location && !asDashboard && !customLocation)) || (auth && !orderState?.options?.user_id)) return
+    if (
+      (orderState.loading || (!orderState.options?.address?.location && !asDashboard && !customLocation)) ||
+      (auth && !orderState?.options?.user_id)
+    ) return
     if (!isDoordash && !franchiseId) {
       getBusinesses(true, currentPageParam)
     }
-  }, [JSON.stringify(orderState.options), orderState.loading, businessTypeSelected, priceLevelSelected, searchValue, initialPricelevel, initialBuisnessType, timeLimitValue, orderByValue, maxDeliveryFee, businessId])
+  }, [
+    JSON.stringify(orderState.options),
+    orderState.loading,
+    businessTypeSelected,
+    priceLevelSelected,
+    searchValue,
+    initialPricelevel,
+    initialBuisnessType,
+    timeLimitValue,
+    orderByValue,
+    maxDeliveryFee,
+    businessId
+  ])
 
   useEffect(() => {
     if ((orderState.loading || (!orderState.options?.address?.location && !asDashboard && !customLocation))) {
@@ -410,6 +424,7 @@ export const BusinessList = (props) => {
   }, [initialFilterKey, initialFilterValue])
 
   useEffect(() => {
+    if (citiesState.loading) return
     if (showCities) {
       getCities()
     }
