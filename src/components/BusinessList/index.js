038ab686061e82bd4b9fad9c7406edeabd44d7ff
Jesus David <50583541+YisusJe@users.noreply.github.com>
2022-12-06 09:56:35 -0400

    [ADD] effect for header search bar (#840)
    Co-authored-by: DESKTOP-S4I8S56\Jesus <jesus@ordering.co>

--- src/components/BusinessList/index.js
+++ src/components/BusinessList/index.js
@@ -34,7 +34,8 @@ export const BusinessList = (props) => {
     franchiseId,
     businessId,
     cityId,
-    actualSlug
+    actualSlug,
+    searchValueCustom
   } = props
 
   const [businessesList, setBusinessesList] = useState({ businesses: [], loading: true, error: null })
@@ -427,6 +428,10 @@ export const BusinessList = (props) => {
       getCities()
     }
   }, [showCities])
+
+  useEffect(() => {
+    handleChangeSearch(searchValueCustom)
+  }, [searchValueCustom])
 
   /**
    * Default behavior business click
