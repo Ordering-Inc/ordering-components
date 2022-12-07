50cb74106c12a378aa5f72443a5df9447da3dfef
Carlos Quiroz <carloseqv29596@gmail.com>
2022-12-07 13:29:02 -0400

    build

--- _modules/components/PhoneAutocomplete/index.js
+++ _modules/components/PhoneAutocomplete/index.js
@@ -334,7 +334,7 @@ var PhoneAutocomplete = function PhoneAutocomplete(props) {
   }();
   (0, _react.useEffect)(function () {
     var _customersPhones$user;
-    if (phone && phone.length === 7 && (customersPhones === null || customersPhones === void 0 ? void 0 : (_customersPhones$user = customersPhones.users) === null || _customersPhones$user === void 0 ? void 0 : _customersPhones$user.length) === 0) {
+    if (phone && phone.length >= 7 && (customersPhones === null || customersPhones === void 0 ? void 0 : (_customersPhones$user = customersPhones.users) === null || _customersPhones$user === void 0 ? void 0 : _customersPhones$user.length) === 0) {
       getUsers();
     }
     if (phone && phone.length < 7) {
