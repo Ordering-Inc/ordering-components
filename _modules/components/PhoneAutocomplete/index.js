01352907bbc03c18cf9729d4d50f9ba72c431808
Carlos Quiroz <carloseqv29596@gmail.com>
2022-12-08 15:50:01 -0400

    build

--- _modules/components/PhoneAutocomplete/index.js
+++ _modules/components/PhoneAutocomplete/index.js
@@ -337,7 +337,7 @@ var PhoneAutocomplete = function PhoneAutocomplete(props) {
     if (phone && phone.length >= 7 && (customersPhones === null || customersPhones === void 0 ? void 0 : (_customersPhones$user = customersPhones.users) === null || _customersPhones$user === void 0 ? void 0 : _customersPhones$user.length) === 0) {
       getUsers();
     }
-    if (phone && phone.length < 7) {
+    if (phone && phone.length < 7 || !phone) {
       setCustomersPhones(_objectSpread(_objectSpread({}, customersPhones), {}, {
         users: []
       }));
