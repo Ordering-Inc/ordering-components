2c3ed1b9b69a231567947cdf62bc2e1eb4339d98
Carlos Quiroz <carloseqv29596@gmail.com>
2022-12-20 11:15:24 -0400

    build

--- _modules/components/OrderListGroups/index.js
+++ _modules/components/OrderListGroups/index.js
@@ -36,7 +36,7 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0) { ; } } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 var OrderListGroups = function OrderListGroups(props) {
-  var _configs$logistic_mod, _orderGroupStatusCust, _orderGroupStatusCust2, _orderGroupStatusCust3, _orderGroupStatusCust4, _paginationSettings$p;
+  var _configs$logistic_mod, _orderGroupStatusCust, _orderGroupStatusCust2, _orderGroupStatusCust3, _orderGroupStatusCust4, _paginationSettings$p, _ordersGroup$currentT19;
   var UIComponent = props.UIComponent,
     orderBy = props.orderBy,
     isIos = props.isIos,
@@ -1338,7 +1338,7 @@ var OrderListGroups = function OrderListGroups(props) {
     onFiltered: setFiltered,
     handleChangeOrderStatus: handleChangeOrderStatus,
     handleSendCustomerReview: handleSendCustomerReview,
-    ordersGroupedFormatted: formatOrdersGrouped(ordersGroup[currentTabSelected].ordersGrouped),
+    ordersGroupedFormatted: formatOrdersGrouped((_ordersGroup$currentT19 = ordersGroup[currentTabSelected]) === null || _ordersGroup$currentT19 === void 0 ? void 0 : _ordersGroup$currentT19.ordersGrouped),
     isLogisticActivated: isLogisticActivated
   })));
 };
