4a20ed0cf9d6334b289218406fa02a3e73ef2214
Carlos Quiroz <carloseqv29596@gmail.com>
2022-12-28 10:07:39 -0400

    build

--- _modules/components/ProductOptionSuboption/index.js
+++ _modules/components/ProductOptionSuboption/index.js
@@ -80,7 +80,7 @@ var ProductOptionSuboption = function ProductOptionSuboption(props) {
     if (option.limit_suboptions_by_max && (balance === option.max || state.quantity === suboption.max)) {
       return;
     }
-    if (!option.limit_suboptions_by_max && state.quantity === option.max) {
+    if (!option.limit_suboptions_by_max && state.quantity === suboption.max) {
       return;
     }
     changeState(_objectSpread(_objectSpread({}, state), {}, {
