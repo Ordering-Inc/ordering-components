ca8c4ed0fdfe8ef6c754f813d38bd3f44ad267e3
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2023-02-13 13:30:00 -0400

    [FEAT] improve billing context (#921)
    Co-authored-by: Carlos Quiroz <carloseqv29596@gmail.com>

--- src/contexts/BillingContext/index.js
+++ src/contexts/BillingContext/index.js
@@ -1,13 +1,7 @@
 import React, { createContext } from 'react'
-import { ConfigProvider } from '../ConfigContext'
-import { SessionProvider } from '../SessionContext'
-import { LanguageProvider } from '../LanguageContext'
 import { ApiProvider } from '../ApiContext'
 import { EventProvider } from '../EventContext'
 import { ToastProvider } from '../ToastContext'
-import { WebStrategy } from '../../webStrategy'
-import { OrderProvider } from '../OrderContext'
-import { OptimizationLoadProvider } from '../OptimizationLoadContext'
 /**
  * Create BillingContext
  * Wrapper to use all context to ordering apps
@@ -19,34 +13,14 @@ export const BillingContext = createContext()
  * This provider has a reducer for manage languages state
  * @param {props} props
  */
-export const BillingProvider = ({ Alert, settings, children }) => {
-  const webStrategy = new WebStrategy()
-  const restOfSettings = {
-    project: settings.project,
-    appId: settings.app_id,
-    countryCode: settings.countryCode,
-    useOptimizeLoad: settings.useOptimizeLoad
-  }
+export const BillingProvider = ({ settings, children }) => {
   return (
     <BillingContext.Provider>
       <EventProvider>
         <ApiProvider settings={Object.assign(settings.api, { project: settings.project, appId: settings.app_id })}>
-          <OptimizationLoadProvider settings={Object.assign(settings.api, restOfSettings)}>
-            <LanguageProvider strategy={webStrategy}>
-              <ConfigProvider strategy={webStrategy}>
-                <ToastProvider>
-                  <SessionProvider strategy={webStrategy}>
-                    <OrderProvider
-                      strategy={webStrategy}
-                      Alert={Alert}
-                    >
-                      {children}
-                    </OrderProvider>
-                  </SessionProvider>
-                </ToastProvider>
-              </ConfigProvider>
-            </LanguageProvider>
-          </OptimizationLoadProvider>
+          <ToastProvider>
+            {children}
+          </ToastProvider>
         </ApiProvider>
       </EventProvider>
     </BillingContext.Provider>
