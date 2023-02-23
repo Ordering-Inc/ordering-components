1dad39d84944b076ebe396aef65330fe003e2a99
Carlos Quiroz <carloseqv29596@gmail.com>
2023-02-23 15:08:42 -0400

    build

--- _modules/components/PromotionsController/index.js
+++ _modules/components/PromotionsController/index.js
@@ -28,8 +28,10 @@ function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len 
 function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i.return && (_r = _i.return(), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 var PromotionsController = function PromotionsController(props) {
+  var _options$address;
   var UIComponent = props.UIComponent,
-    paramsToFetch = props.paramsToFetch;
+    paramsToFetch = props.paramsToFetch,
+    franchiseId = props.franchiseId;
   var _useSession = (0, _SessionContext.useSession)(),
     _useSession2 = _slicedToArray(_useSession, 1),
     session = _useSession2[0];
@@ -56,16 +58,19 @@ var PromotionsController = function PromotionsController(props) {
     _useState6 = _slicedToArray(_useState5, 2),
     offersState = _useState6[0],
     setOffersState = _useState6[1];
+  var location = JSON.stringify(options === null || options === void 0 ? void 0 : (_options$address = options.address) === null || _options$address === void 0 ? void 0 : _options$address.location);
   var getOffers = /*#__PURE__*/function () {
     var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee() {
-      var _options$address;
-      var location, url, response;
+      var params, url, response;
       return _regeneratorRuntime().wrap(function _callee$(_context) {
         while (1) switch (_context.prev = _context.next) {
           case 0:
-            location = JSON.stringify(options === null || options === void 0 ? void 0 : (_options$address = options.address) === null || _options$address === void 0 ? void 0 : _options$address.location);
-            url = "".concat(ordering.root, "/offers/public?enabled=true&params=").concat(paramsToFetch.join(), "&location=").concat(location);
-            _context.next = 4;
+            params = "?enabled=true&params=".concat(paramsToFetch.join(), "&location=").concat(location, "&order_type_id=").concat(options === null || options === void 0 ? void 0 : options.type);
+            if (franchiseId) {
+              params = params + "&franchise_id=".concat(franchiseId);
+            }
+            url = "".concat(ordering.root, "/offers/public").concat(params);
+            _context.next = 5;
             return fetch(url, {
               method: 'GET',
               headers: {
@@ -75,13 +80,13 @@ var PromotionsController = function PromotionsController(props) {
                 'X-Socket-Id-X': socket === null || socket === void 0 ? void 0 : socket.getId()
               }
             });
-          case 4:
+          case 5:
             response = _context.sent;
-            _context.next = 7;
+            _context.next = 8;
             return response.json();
-          case 7:
-            return _context.abrupt("return", _context.sent);
           case 8:
+            return _context.abrupt("return", _context.sent);
+          case 9:
           case "end":
             return _context.stop();
         }
@@ -136,7 +141,7 @@ var PromotionsController = function PromotionsController(props) {
   };
   (0, _react.useEffect)(function () {
     loadOffers();
-  }, []);
+  }, [JSON.stringify(location), options === null || options === void 0 ? void 0 : options.type]);
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, UIComponent && /*#__PURE__*/_react.default.createElement(UIComponent, _extends({}, props, {
     offersState: offersState,
     searchValue: searchValue,
