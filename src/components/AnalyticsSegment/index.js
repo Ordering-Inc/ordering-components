7c334dd116739115a27b2a35f68319e5ffeadba4
carlosg-ordering <carlos.g@ordering.co>
2023-03-07 12:31:23 -0600

    [FEAT] segment events

--- src/components/AnalyticsSegment/index.js
+++ src/components/AnalyticsSegment/index.js
@@ -13,7 +13,7 @@ export const AnalyticsSegment = (props) => {
   const [events] = useEvent()
   const [analytics, setAnalytics] = useState(undefined)
 
-  const handleClickProduct = (product) => {
+  const handleClickProduct = (product, featured) => {
     console.log('Segment Product Clicked', product)
     analytics.track('Product Clicked', {
       id: product.id,
@@ -21,6 +21,15 @@ export const AnalyticsSegment = (props) => {
       category: product.category_id,
       price: product.price
     })
+    if (featured) {
+      console.log('Promotion Clicked', product)
+      analytics.track('Promotion Clicked', {
+        id: product.id,
+        name: product.name,
+        category: product.category_id,
+        price: product.price
+      })
+    }
   }
 
   const handleProductAdded = (product) => {
@@ -48,11 +57,16 @@ export const AnalyticsSegment = (props) => {
   const handleOrderPlaced = (order) => {
     console.log('Segment Order Placed', order)
     analytics.track('Order Placed', {
-      id: order.id,
-      affiliation: order.business?.name,
-      revenue: order.total,
-      tax: order.tax_total,
-      shipping: order.delivery_zone_price
+      total: order?.total,
+      business_id: order?.business_id,
+      customer_email: order?.customer?.email,
+      customer_phone: order?.customer?.cellphone,
+      coupon: order?.coupon || 'NA',
+      coupon_price: order?.offer || 'NA',
+      marca: order?.app_id,
+      nombre_de_la_tienda: order?.business?.name,
+      order: order?.id,
+      products: order?.products
     })
     analytics.track('Payment Info Entered', {
       order: order.id,
@@ -95,6 +109,31 @@ export const AnalyticsSegment = (props) => {
     })
   }
 
+  const handleCategorySelect = (data) => {
+    console.log('Category Clicked', data)
+    analytics.track('Category Clicked', {
+      category: data?.params?.category.name
+    })
+  }
+
+  const handleInCheckout = (data) => {
+    console.log('Checkout Started', data)
+    analytics.track('Checkout Started', {
+      nombre_de_la_tienda: data?.business?.name,
+      total: data?.total,
+      products: data?.products
+    })
+  }
+
+  const handleGoToBusiness = (storeData) => {
+    if (storeData?.params?.store !== ':store' && storeData?.params?.store !== 'undefined') {
+      console.log('Checkout Started', storeData)
+      analytics.track('Abrir negocio', {
+        restaurant: storeData?.params?.store
+      })
+    }
+  }
+
   useEffect(() => {
     if (analytics && !customData) {
       events.on('product_clicked', handleClickProduct)
@@ -102,44 +141,48 @@ export const AnalyticsSegment = (props) => {
       events.on('userLogin', handleLogin)
       events.on('order_placed', handleOrderPlaced)
       events.on('order_updated', handleUpdateOrder)
-      events.on('order_added', handleAddOrder)
+      // events.on('order_added', handleAddOrder)
       events.on('cart_product_removed', handleProductRemoved)
+      events.on('category_selected', handleCategorySelect)
+      events.on('product_promotion_clicked', handleCategorySelect)
+      events.on('in-checkout', handleInCheckout)
+      events.on('go_to_business', handleGoToBusiness)
     }
     if (!customData || !analytics) return
     const { type, data } = customData
     switch (type) {
-			case 'Aplicar Cupon':
+      case 'Aplicar Cupon':
         console.log('Segment Aplicar Cupon', data)
-				analytics.track('Aplicar Cupon', {
-					id: data.id,
-					name: data.name
-				})
-				break;
-			case 'Calificar Orden':
+        analytics.track('Aplicar Cupon', {
+          id: data.id,
+          name: data.name
+        })
+        break
+      case 'Calificar Orden':
         console.log('Segment Calificar Orden', data)
-				analytics.track('Calificar Orden', {
-					business_id: data.business_id,
-					order_id: data.order_id,
-					user_id: data.user_id,
-				})
-				break;
-			case 'Abrir banner':
+        analytics.track('Calificar Orden', {
+          business_id: data.business_id,
+          order_id: data.order_id,
+          user_id: data.user_id
+        })
+        break
+      case 'Abrir banner':
         console.log('Segment Abrir banner', data)
-				analytics.track('Abrir banner', {
-					type: data.type,
-					brand: data.brand
-				})
-				break;
-			case 'Abrir negocio':
+        analytics.track('Abrir banner', {
+          type: data.type,
+          brand: data.brand
+        })
+        break
+      case 'Abrir negocio':
         console.log('Segment Abrir negocio', data)
-				analytics.track('Abrir negocio', {
-					id: data.id,
-					name: data.name,
-				})
-				break;
-			default:
-				break;
-		}
+        analytics.track('Abrir negocio', {
+          id: data.id,
+          name: data.name
+        })
+        break
+      default:
+        break
+    }
     return () => {
       if (analytics) {
         events.off('product_clicked', handleClickProduct)
@@ -147,8 +190,12 @@ export const AnalyticsSegment = (props) => {
         events.off('userLogin', handleLogin)
         events.off('order_placed', handleOrderPlaced)
         events.off('order_updated', handleUpdateOrder)
-        events.off('order_added', handleAddOrder)
+        // events.off('order_added', handleAddOrder)
         events.off('cart_product_removed', handleProductRemoved)
+        events.off('category_selected', handleCategorySelect)
+        events.off('product_promotion_clicked', handleCategorySelect)
+        events.off('in-checkout', handleInCheckout)
+        events.off('go_to_business', handleGoToBusiness)
       }
     }
   }, [analytics, customData])
