852716cff39b1e788ae0255fbbfa8b7ba8fc7254
Israel Rojas <israelrojas626@gmail.com>
2023-03-09 15:03:09 -0400

    dinamic promise all implemented

--- src/components/ReviewDriver/index.js
+++ src/components/ReviewDriver/index.js
@@ -22,72 +22,39 @@ export const ReviewDriver = (props) => {
    */
   const handleSendDriverReview = async () => {
     setFormState({ ...formState, loading: true })
-    const userId = isProfessional
-      ? order?.products[0]?.calendar_event?.professional?.id
-      : order?.driver?.id
-    if (!order?.id.length) {
-      try {
-        const response = await fetch(`${ordering.root}/users/${userId}/user_reviews`, {
-          method: 'POST',
-          headers: {
-            Authorization: `Bearer ${session.token}`,
-            'Content-Type': 'application/json',
-            'X-App-X': ordering.appId,
-            'X-Socket-Id-X': socket?.getId()
-          },
-          body: JSON.stringify({ ...reviews, order_id: order?.id, user_id: userId })
-        })
-        const { result, error } = await response.json()
-        if (!error) {
-          setFormState({
-            loading: false,
-            result: {
-              result: result,
-              error: false
-            }
+    const orderId = Array.isArray(order?.id) ? order?.id : [order?.id]
+    if (!orderId?.length) return
+
+    try {
+      const userId = isProfessional
+        ? order?.products[0]?.calendar_event?.professional?.id
+        : order?.driver?.id
+
+        const fetchOrders = async (ids) => {
+          const promises = ids.map(async id => {
+            const res = await fetch(`${ordering.root}/users/${userId}/user_reviews`, {
+              method: 'POST',
+              headers: {
+                Authorization: `Bearer ${session.token}`,
+                'Content-Type': 'application/json',
+                'X-App-X': ordering.appId,
+                'X-Socket-Id-X': socket?.getId()
+              },
+              body: JSON.stringify({ ...reviews, order_id: id, user_id: userId })
+            })
+            const { result, error } = await res.json()
+            return { result, error }
           })
-          if (isToast) {
-            showToast(
-              ToastType.Success,
-              isProfessional
-                ? t('PROFESSIONAL_REVIEW_SUCCESS_CONTENT', 'Thank you, Professional review successfully submitted!')
-                : t('DRIVER_REVIEW_SUCCESS_CONTENT', 'Thank you, Driver review successfully submitted!'))
-          }
-        } else {
-          setFormState({
-            ...formState,
-            loading: false,
-            result: {
-              result: result,
-              error: true
-            }
-          })
+          const data = await Promise.all(promises)
+          return data
         }
-      } catch (err) {
-        setFormState({
-          ...formState,
-          result: {
-            error: true,
-            result: err.message
-          },
-          loading: false
-        })
-      }
-    } else {
-      let requests = order?.id?.map((orderId) => {
-        return fetch(`${ordering.root}/users/${userId}/user_reviews`, {
-          method: 'POST',
-          headers: {
-            Authorization: `Bearer ${session.token}`,
-            'Content-Type': 'application/json',
-            'X-App-X': ordering.appId,
-            'X-Socket-Id-X': socket?.getId()
-          },
-          body: JSON.stringify({ ...reviews, order_id: orderId, user_id: userId })
-        })
-      })
-      await Promise.all(requests.map(response => response.then(async (response) => {
-        const { result } = await response.json()
+
+      const reviewsArray = await fetchOrders(orderId)
+
+      const error = reviewsArray.length && reviewsArray.every(obj => obj.error) && reviewsArray[0]?.result?.[0]
+      const result = reviewsArray.length && reviewsArray.map(obj => (obj.result?.[0] ?? obj.result)).filter(o => typeof o !== 'string')
+
+      if (!error) {
         setFormState({
           loading: false,
           result: {
@@ -102,20 +69,28 @@ export const ReviewDriver = (props) => {
               ? t('PROFESSIONAL_REVIEW_SUCCESS_CONTENT', 'Thank you, Professional review successfully submitted!')
               : t('DRIVER_REVIEW_SUCCESS_CONTENT', 'Thank you, Driver review successfully submitted!'))
         }
-      })
-        .catch(error => {
-          setFormState({
-            ...formState,
-            loading: false,
-            result: {
-              result: error,
-              error: true
-            }
-          })
+      } else {
+        setFormState({
+          ...formState,
+          loading: false,
+          result: {
+            result: result,
+            error: true
+          }
         })
-      ))
+      }
+    } catch (err) {
+      setFormState({
+        ...formState,
+        result: {
+          error: true,
+          result: err.message
+        },
+        loading: false
+      })
     }
   }
+
   return (
     <>
       {UIComponent && (
