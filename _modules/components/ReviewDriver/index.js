74d3a2710d6d305c7fc7c149184c7f2320ad435b
Carlos Quiroz <carloseqv29596@gmail.com>
2023-03-09 17:06:39 -0400

    build

--- _modules/components/ReviewDriver/index.js
+++ _modules/components/ReviewDriver/index.js
@@ -69,38 +69,99 @@ var ReviewDriver = function ReviewDriver(props) {
    * Function that load and send the driver review to ordering
    */
   var handleSendDriverReview = /*#__PURE__*/function () {
-    var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee() {
-      var _order$products$, _order$products$$cale, _order$products$$cale2, _order$driver, userId, response, _yield$response$json, result, error;
-      return _regeneratorRuntime().wrap(function _callee$(_context) {
-        while (1) switch (_context.prev = _context.next) {
+    var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee3() {
+      var orderId, _order$products$, _order$products$$cale, _order$products$$cale2, _order$driver, _reviewsArray$, _reviewsArray$$result, userId, fetchReviews, reviewsArray, error, result;
+      return _regeneratorRuntime().wrap(function _callee3$(_context3) {
+        while (1) switch (_context3.prev = _context3.next) {
           case 0:
             setFormState(_objectSpread(_objectSpread({}, formState), {}, {
               loading: true
             }));
-            _context.prev = 1;
+            orderId = Array.isArray(order === null || order === void 0 ? void 0 : order.id) ? order === null || order === void 0 ? void 0 : order.id : [order === null || order === void 0 ? void 0 : order.id];
+            if (orderId !== null && orderId !== void 0 && orderId.length) {
+              _context3.next = 4;
+              break;
+            }
+            return _context3.abrupt("return");
+          case 4:
+            _context3.prev = 4;
             userId = isProfessional ? order === null || order === void 0 ? void 0 : (_order$products$ = order.products[0]) === null || _order$products$ === void 0 ? void 0 : (_order$products$$cale = _order$products$.calendar_event) === null || _order$products$$cale === void 0 ? void 0 : (_order$products$$cale2 = _order$products$$cale.professional) === null || _order$products$$cale2 === void 0 ? void 0 : _order$products$$cale2.id : order === null || order === void 0 ? void 0 : (_order$driver = order.driver) === null || _order$driver === void 0 ? void 0 : _order$driver.id;
-            _context.next = 5;
-            return fetch("".concat(ordering.root, "/users/").concat(userId, "/user_reviews"), {
-              method: 'POST',
-              headers: {
-                Authorization: "Bearer ".concat(session.token),
-                'Content-Type': 'application/json',
-                'X-App-X': ordering.appId,
-                'X-Socket-Id-X': socket === null || socket === void 0 ? void 0 : socket.getId()
-              },
-              body: JSON.stringify(_objectSpread(_objectSpread({}, reviews), {}, {
-                order_id: order === null || order === void 0 ? void 0 : order.id,
-                user_id: userId
-              }))
+            fetchReviews = /*#__PURE__*/function () {
+              var _ref2 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee2(ids) {
+                var promises, data;
+                return _regeneratorRuntime().wrap(function _callee2$(_context2) {
+                  while (1) switch (_context2.prev = _context2.next) {
+                    case 0:
+                      promises = ids.map( /*#__PURE__*/function () {
+                        var _ref3 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee(id) {
+                          var res, _yield$res$json, result, error;
+                          return _regeneratorRuntime().wrap(function _callee$(_context) {
+                            while (1) switch (_context.prev = _context.next) {
+                              case 0:
+                                _context.next = 2;
+                                return fetch("".concat(ordering.root, "/users/").concat(userId, "/user_reviews"), {
+                                  method: 'POST',
+                                  headers: {
+                                    Authorization: "Bearer ".concat(session.token),
+                                    'Content-Type': 'application/json',
+                                    'X-App-X': ordering.appId,
+                                    'X-Socket-Id-X': socket === null || socket === void 0 ? void 0 : socket.getId()
+                                  },
+                                  body: JSON.stringify(_objectSpread(_objectSpread({}, reviews), {}, {
+                                    order_id: id,
+                                    user_id: userId
+                                  }))
+                                });
+                              case 2:
+                                res = _context.sent;
+                                _context.next = 5;
+                                return res.json();
+                              case 5:
+                                _yield$res$json = _context.sent;
+                                result = _yield$res$json.result;
+                                error = _yield$res$json.error;
+                                return _context.abrupt("return", {
+                                  result: result,
+                                  error: error
+                                });
+                              case 9:
+                              case "end":
+                                return _context.stop();
+                            }
+                          }, _callee);
+                        }));
+                        return function (_x3) {
+                          return _ref3.apply(this, arguments);
+                        };
+                      }());
+                      _context2.next = 3;
+                      return Promise.all(promises);
+                    case 3:
+                      data = _context2.sent;
+                      return _context2.abrupt("return", data);
+                    case 5:
+                    case "end":
+                      return _context2.stop();
+                  }
+                }, _callee2);
+              }));
+              return function fetchReviews(_x2) {
+                return _ref2.apply(this, arguments);
+              };
+            }();
+            _context3.next = 9;
+            return fetchOrders(orderId);
+          case 9:
+            reviewsArray = _context3.sent;
+            error = reviewsArray.length && reviewsArray.every(function (obj) {
+              return obj.error;
+            }) && ((_reviewsArray$ = reviewsArray[0]) === null || _reviewsArray$ === void 0 ? void 0 : (_reviewsArray$$result = _reviewsArray$.result) === null || _reviewsArray$$result === void 0 ? void 0 : _reviewsArray$$result[0]);
+            result = reviewsArray.length && reviewsArray.map(function (obj) {
+              var _obj$result$, _obj$result;
+              return (_obj$result$ = (_obj$result = obj.result) === null || _obj$result === void 0 ? void 0 : _obj$result[0]) !== null && _obj$result$ !== void 0 ? _obj$result$ : obj.result;
+            }).filter(function (o) {
+              return typeof o !== 'string';
             });
-          case 5:
-            response = _context.sent;
-            _context.next = 8;
-            return response.json();
-          case 8:
-            _yield$response$json = _context.sent;
-            result = _yield$response$json.result;
-            error = _yield$response$json.error;
             if (!error) {
               setFormState({
                 loading: false,
@@ -121,23 +182,23 @@ var ReviewDriver = function ReviewDriver(props) {
                 }
               }));
             }
-            _context.next = 17;
+            _context3.next = 18;
             break;
-          case 14:
-            _context.prev = 14;
-            _context.t0 = _context["catch"](1);
+          case 15:
+            _context3.prev = 15;
+            _context3.t0 = _context3["catch"](4);
             setFormState(_objectSpread(_objectSpread({}, formState), {}, {
               result: {
                 error: true,
-                result: _context.t0.message
+                result: _context3.t0.message
               },
               loading: false
             }));
-          case 17:
+          case 18:
           case "end":
-            return _context.stop();
+            return _context3.stop();
         }
-      }, _callee, null, [[1, 14]]);
+      }, _callee3, null, [[4, 15]]);
     }));
     return function handleSendDriverReview() {
       return _ref.apply(this, arguments);
