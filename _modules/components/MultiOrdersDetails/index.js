a44482c5b00938187e339eb4be81acf3e4a90e92
Carlos Quiroz <carloseqv29596@gmail.com>
2023-03-20 14:33:06 -0400

    build

--- _modules/components/MultiOrdersDetails/index.js
+++ _modules/components/MultiOrdersDetails/index.js
@@ -21,6 +21,10 @@ function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _ty
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
 function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
@@ -31,7 +35,7 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
  * Component to manage Multi orders page behavior without UI component
  */
 var MultiOrdersDetails = function MultiOrdersDetails(props) {
-  var _ordersList$orders$, _ordersList$orders$2;
+  var _ordersList$orders$;
   var orderId = props.orderId,
     userId = props.userId,
     onRedirectPage = props.onRedirectPage,
@@ -54,12 +58,27 @@ var MultiOrdersDetails = function MultiOrdersDetails(props) {
     _useState4 = _slicedToArray(_useState3, 2),
     ordersSummary = _useState4[0],
     setOrdersSummary = _useState4[1];
+  var formatPaymentEvents = function formatPaymentEvents(payments) {
+    var _ref;
+    var list = (_ref = []).concat.apply(_ref, _toConsumableArray(payments));
+    var uniqueArr = [];
+    var uniqueObj = {};
+    list.forEach(function (item) {
+      var _item$wallet_event;
+      if (!uniqueObj[item === null || item === void 0 ? void 0 : (_item$wallet_event = item.wallet_event) === null || _item$wallet_event === void 0 ? void 0 : _item$wallet_event.id]) {
+        var _item$wallet_event2;
+        uniqueObj[item === null || item === void 0 ? void 0 : (_item$wallet_event2 = item.wallet_event) === null || _item$wallet_event2 === void 0 ? void 0 : _item$wallet_event2.id] = true;
+        uniqueArr.push(item);
+      }
+    });
+    return uniqueArr !== null && uniqueArr !== void 0 ? uniqueArr : [];
+  };
 
   /**
    * Get orders from API
    */
   var getOrders = /*#__PURE__*/function () {
-    var _ref = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee() {
+    var _ref2 = _asyncToGenerator( /*#__PURE__*/_regeneratorRuntime().mark(function _callee() {
       var options, _yield$ordering$setAc, _yield$ordering$setAc2, error, result, _total, _subtotal, _tax;
       return _regeneratorRuntime().wrap(function _callee$(_context) {
         while (1) switch (_context.prev = _context.next) {
@@ -127,7 +146,7 @@ var MultiOrdersDetails = function MultiOrdersDetails(props) {
       }, _callee, null, [[0, 14]]);
     }));
     return function getOrders() {
-      return _ref.apply(this, arguments);
+      return _ref2.apply(this, arguments);
     };
   }();
   (0, _react.useEffect)(function () {
@@ -140,7 +159,9 @@ var MultiOrdersDetails = function MultiOrdersDetails(props) {
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, UIComponent && /*#__PURE__*/_react.default.createElement(UIComponent, _extends({}, props, {
     ordersList: ordersList,
     customer: (_ordersList$orders$ = ordersList.orders[0]) === null || _ordersList$orders$ === void 0 ? void 0 : _ordersList$orders$.customer,
-    paymentEvents: ((_ordersList$orders$2 = ordersList.orders[0]) === null || _ordersList$orders$2 === void 0 ? void 0 : _ordersList$orders$2.payment_events) || [],
+    paymentEvents: formatPaymentEvents(ordersList.orders.map(function (order) {
+      return order === null || order === void 0 ? void 0 : order.payment_events;
+    }) || []),
     ordersSummary: ordersSummary
   })));
 };
