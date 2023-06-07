a87ddb136f2483f321bf4955a0ab868607ef8d65
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2023-06-08 03:04:07 +0800

    [Update] banner functionality (#994)
    * [Update] banner functionality
    
    * [Update] dependency

--- src/components/PageBanner/index.js
+++ src/components/PageBanner/index.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { useApi } from '../../contexts/ApiContext'
 import { useWebsocket } from '../../contexts/WebsocketContext'
+import { useConfig } from '../../contexts/ConfigContext'
+import { useOrder } from '../../contexts/OrderContext'
 
 export const PageBanner = (props) => {
   const {
     UIComponent,
-    position
+    position,
+    businessId
   } = props
 
   const [ordering] = useApi()
   const socket = useWebsocket()
+  const [{ configs }] = useConfig()
+  const [orderState] = useOrder()
+
   const [pageBannerState, setPageBannerState] = useState({ loading: true, banner: null, error: null })
+
+  const unaddressedTypes = configs?.unaddressed_order_types_allowed?.value.split('|').map(value => Number(value)) || []
+  const isAllowUnaddressOrderType = unaddressedTypes.includes(orderState?.options?.type)
 
   /**
    * Method to get the page banner from API
@@ -27,7 +36,25 @@ export const PageBanner = (props) => {
           'X-Socket-Id-X': socket?.getId()
         }
       }
-      const response = await fetch(`${ordering.root}/banner?position=${position}`, requestOptions)
+      let fetchEndpoint = `${ordering.root}/banner?position=${position}`
+      if (position === 'web_business_listing' || position === 'app_business_listing') {
+        const defaultLatitude = Number(configs?.location_default_latitude?.value)
+        const defaultLongitude = Number(configs?.location_default_longitude?.value)
+        const isInvalidDefaultLocation = isNaN(defaultLatitude) || isNaN(defaultLongitude)
+        const defaultLocation = {
+          lat: !isInvalidDefaultLocation ? defaultLatitude : 40.7744146,
+          lng: !isInvalidDefaultLocation ? defaultLongitude : -73.9678064
+        }
+        const location = (isAllowUnaddressOrderType && !orderState.options?.address?.location)
+          ? defaultLocation
+          : `${orderState.options?.address?.location}`
+        const type = orderState.options?.type || 1
+        fetchEndpoint = `${ordering.root}/banner?position=${position}&location=${JSON.stringify(location)}&order_type_id=${type}`
+      }
+      if (position === 'web_business_page' || position === 'app_business_page') {
+        fetchEndpoint = `${ordering.root}/banner?position=${position}&business_id=${businessId}`
+      }
+      const response = await fetch(fetchEndpoint, requestOptions)
       const { error, result } = await response.json()
       const totalItems = result.reduce((items, banner) => [...items, ...banner?.items], [])
       setPageBannerState({
@@ -46,9 +73,9 @@ export const PageBanner = (props) => {
   }
 
   useEffect(() => {
-    if (!position) return
+    if (!position || orderState.loading) return
     handleGetPageBanner()
-  }, [position])
+  }, [position, businessId, orderState.options?.address?.location, orderState.options?.type])
 
   return (
     <>
