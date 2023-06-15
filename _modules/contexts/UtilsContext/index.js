7a7556648731bbe0a6c8547b9df306bebb194ce2
YisusJe <jesus@ordering.co>
2023-06-14 21:27:07 -0400

    build

--- _modules/contexts/UtilsContext/index.js
+++ _modules/contexts/UtilsContext/index.js
@@ -8,6 +8,7 @@ exports.useUtils = exports.UtilsProviders = exports.UtilsContext = void 0;
 var _react = _interopRequireWildcard(require("react"));
 var _ConfigContext = require("../ConfigContext");
 var _LanguageContext = require("../LanguageContext");
+var _moment = _interopRequireDefault(require("moment"));
 var _dayjs = _interopRequireDefault(require("dayjs"));
 var _utc = _interopRequireDefault(require("dayjs/plugin/utc"));
 var _relativeTime = _interopRequireDefault(require("dayjs/plugin/relativeTime"));
@@ -50,24 +51,15 @@ var UtilsProviders = function UtilsProviders(_ref) {
   // const [localObject, setLocalObject] = useState({})
 
   var refreshLocalObject = function refreshLocalObject() {
-    var months = [t('MONTH1', 'January'), t('MONTH2', 'February'), t('MONTH3', 'March'), t('MONTH4', 'April'), t('MONTH5', 'May'), t('MONTH6', 'June'), t('MONTH7', 'July'), t('MONTH8', 'August'), t('MONTH9', 'September'), t('MONTH10', 'October'), t('MONTH11', 'November'), t('MONTH12', 'December')];
-    var monthsShort = [t('MONTHSHORT1', 'Jan'), t('MONTHSHORT2', 'Feb'), t('MONTHSHORT3', 'Mar'), t('MONTHSHORT4', 'Apr'), t('MONTHSHORT5', 'May'), t('MONTHSHORT6', 'Jun'), t('MONTHSHORT7', 'Jul'), t('MONTHSHORT8', 'Aug'), t('MONTHSHORT9', 'Sep'), t('MONTHSHORT10', 'Oct'), t('MONTHSHORT11', 'Nov'), t('MONTHSHORT12', 'Dec')];
     var weekdays = [t('DAY7', 'Sunday'), t('DAY1', 'Monday'), t('DAY2', 'Tuesday'), t('DAY3', 'Wednesday'), t('DAY4', 'Thursday'), t('DAY5', 'Friday'), t('DAY6', 'Saturday')];
-    var weekdaysShort = [t('DAYSHORT7', 'Sun'), t('DAYSHORT1', 'Mon'), t('DAYSHORT2', 'Tue'), t('DAYSHORT3', 'Wed'), t('DAYSHORT4', 'Thu'), t('DAYSHORT5', 'Fri'), t('DAYSHORT6', 'Sat')];
-    var weekdaysMin = [t('DAYMIN7', 'Su'), t('DAYMIN1', 'Mo'), t('DAYMIN2', 'Tu'), t('DAYMIN3', 'We'), t('DAYMIN4', 'Th'), t('DAYMIN5', 'Fr'), t('DAYMIN6', 'Sa')];
+    var months = [t('MONTH1', 'January'), t('MONTH2', 'February'), t('MONTH3', 'March'), t('MONTH4', 'April'), t('MONTH5', 'May'), t('MONTH6', 'June'), t('MONTH7', 'July'), t('MONTH8', 'August'), t('MONTH9', 'September'), t('MONTH10', 'October'), t('MONTH11', 'November'), t('MONTH12', 'December')];
     var localeObject = {
       name: 'auto',
       // name String
-      months: months,
-      // months Array
-      monthsShort: monthsShort,
-      // monthsShort Array
       weekdays: weekdays,
       // weekdays Array
-      weekdaysShort: weekdaysShort,
-      // weekdaysShort Array
-      weekdaysMin: weekdaysMin,
-      // weekdaysMin Array
+      months: months,
+      // months Array
       ordinal: function ordinal(n) {
         return "".concat(n, "\xBA");
       },
@@ -77,7 +69,7 @@ var UtilsProviders = function UtilsProviders(_ref) {
         future: t('RELATIVE_TIME_IN', 'in %s'),
         // e.g. in 2 hours, %s been replaced with 2hours
         past: t('RELATIVE_TIME_AGO', '%s ago'),
-        s: t('RELATIVE_TIME_FEW_SECONDS', 'a few seconds'),
+        s: t('RELATIVE_TIME_FEW_SECONDS ', 'a few seconds'),
         m: t('RELATIVE_TIME_MINUTE', 'a minute'),
         mm: t('RELATIVE_TIME_MINUTES', '%d minutes'),
         h: t('RELATIVE_TIME_HOUR', 'an hour'),
@@ -92,25 +84,24 @@ var UtilsProviders = function UtilsProviders(_ref) {
       },
       meridiem: function meridiem(hour, minute, isLowercase) {
         // OPTIONAL, AM/PM
-        return hour >= 12 ? t('PM', 'PM') : t('AM', 'AM');
+        return hour > 12 ? t('PM', 'PM') : t('AM', 'AM');
       }
     };
     _dayjs.default.locale('auto', localeObject);
     _dayjs.default.updateLocale('auto', localeObject);
   };
   var parsePrice = function parsePrice(value) {
-    var _configState$configs$, _configState$configs$2, _configState$configs$3, _configState$configs$4, _configState$configs$5, _formatNumber$currenc;
+    var _configState$configs$, _configState$configs$2, _configState$configs$3, _configState$configs$4, _configState$configs$5;
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     var formatNumber = {
       decimal: (options === null || options === void 0 ? void 0 : options.decimal) || ((_configState$configs$ = configState.configs.format_number_decimal_length) === null || _configState$configs$ === void 0 ? void 0 : _configState$configs$.value) || 2,
       separator: (options === null || options === void 0 ? void 0 : options.separator) || ((_configState$configs$2 = configState.configs.format_number_decimal_separator) === null || _configState$configs$2 === void 0 ? void 0 : _configState$configs$2.value) || ',',
       thousand: (options === null || options === void 0 ? void 0 : options.thousand) || ((_configState$configs$3 = configState.configs.format_number_thousand_separator) === null || _configState$configs$3 === void 0 ? void 0 : _configState$configs$3.value) || '.',
       currency: (options === null || options === void 0 ? void 0 : options.currency) || ((_configState$configs$4 = configState.configs.format_number_currency) === null || _configState$configs$4 === void 0 ? void 0 : _configState$configs$4.value) || '$',
-      currencyPosition: (options === null || options === void 0 ? void 0 : options.currencyPosition) || ((_configState$configs$5 = configState.configs.currency_position) === null || _configState$configs$5 === void 0 ? void 0 : _configState$configs$5.value) || 'left',
-      isTruncable: options === null || options === void 0 ? void 0 : options.isTruncable
+      currencyPosition: (options === null || options === void 0 ? void 0 : options.currencyPosition) || ((_configState$configs$5 = configState.configs.currency_position) === null || _configState$configs$5 === void 0 ? void 0 : _configState$configs$5.value) || 'left'
     };
     var number = parseNumber(value, formatNumber);
-    if (((_formatNumber$currenc = formatNumber.currencyPosition) === null || _formatNumber$currenc === void 0 ? void 0 : _formatNumber$currenc.toLowerCase()) === 'left') {
+    if (formatNumber.currencyPosition === 'left') {
       number = formatNumber.currency + ' ' + number;
     } else {
       number = number + ' ' + formatNumber.currency;
@@ -126,17 +117,7 @@ var UtilsProviders = function UtilsProviders(_ref) {
       separator: (options === null || options === void 0 ? void 0 : options.separator) || ((_configState$configs$7 = configState.configs.format_number_decimal_separator) === null || _configState$configs$7 === void 0 ? void 0 : _configState$configs$7.value) || ',',
       thousand: (options === null || options === void 0 ? void 0 : options.thousand) || ((_configState$configs$8 = configState.configs.format_number_thousand_separator) === null || _configState$configs$8 === void 0 ? void 0 : _configState$configs$8.value) || '.'
     };
-    var number = value;
-    if (options !== null && options !== void 0 && options.isTruncable) {
-      var _numberParts$;
-      number = number.toString();
-      var _numberParts = number.split(formatNumber.separator);
-      var decimalPart = (_numberParts$ = _numberParts[1]) !== null && _numberParts$ !== void 0 ? _numberParts$ : '';
-      decimalPart = decimalPart.padEnd(formatNumber.decimal, '0').substring(0, formatNumber.decimal);
-      number = _numberParts[0] + '.' + decimalPart;
-    } else {
-      number = value.toFixed(formatNumber.decimal);
-    }
+    var number = value.toFixed(formatNumber.decimal);
     number = number.toString();
     if (number.indexOf('.')) {
       number = number.replace('.', formatNumber.separator);
@@ -144,30 +125,31 @@ var UtilsProviders = function UtilsProviders(_ref) {
       number = number.replace(',', formatNumber.separator);
     }
     var numberParts = number.split(formatNumber.separator);
+    if (formatNumber.thousand === 'space') formatNumber.thousand = ' ';
     numberParts[0] = numberParts[0].replace(/(.)(?=(\d{3})+$)/g, '$1' + formatNumber.thousand);
     number = numberParts.join(formatNumber.separator);
     return number;
   };
   var parseDate = function parseDate(date) {
-    var _configState$configs$9, _configState$configs, _configState$configs$10;
+    var _configState$configs$9;
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     var formatTime = (options === null || options === void 0 ? void 0 : options.formatTime) || ((_configState$configs$9 = configState.configs.format_time) === null || _configState$configs$9 === void 0 ? void 0 : _configState$configs$9.value) || '24';
     var formatDate = {
       inputFormat: (options === null || options === void 0 ? void 0 : options.inputFormat) || ['YYYY-MM-DD HH:mm:ss', 'YYYY-MM-DD hh:mm:ss A', 'YYYY-MM-DD hh:mm:ss'],
-      outputFormat: (options === null || options === void 0 ? void 0 : options.outputFormat) || ((_configState$configs = configState.configs) === null || _configState$configs === void 0 ? void 0 : (_configState$configs$10 = _configState$configs.dates_general_format) === null || _configState$configs$10 === void 0 ? void 0 : _configState$configs$10.value) || (formatTime === '24' ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD hh:mm:ss A'),
+      outputFormat: (options === null || options === void 0 ? void 0 : options.outputFormat) || (formatTime === '24' ? 'YYYY-MM-DD HH:mm' : 'YYYY-MM-DD hh:mm A'),
       utc: typeof (options === null || options === void 0 ? void 0 : options.utc) === 'boolean' ? options === null || options === void 0 ? void 0 : options.utc : true
     };
-    if (!(0, _dayjs.default)(date, formatDate.inputFormat).isValid()) {
+    if (!(0, _moment.default)(date, formatDate.inputFormat).isValid()) {
       return t('INVALID_FORMAT', 'invalid format');
     }
-    var _date = formatDate.utc ? _dayjs.default.utc(date, formatDate.inputFormat).local() : (0, _dayjs.default)(date, formatDate.inputFormat);
+    var _date = formatDate.utc ? _moment.default.utc(date, formatDate.inputFormat).local() : (0, _moment.default)(date, formatDate.inputFormat);
     return _date.format(formatDate.outputFormat);
   };
   var parseTime = function parseTime(time) {
-    var _configState$configs$11;
+    var _configState$configs$10;
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     if (!time) return '00:00';
-    var _formatTime = (options === null || options === void 0 ? void 0 : options.formatTime) || ((_configState$configs$11 = configState.configs.format_time) === null || _configState$configs$11 === void 0 ? void 0 : _configState$configs$11.value) || '24';
+    var _formatTime = (options === null || options === void 0 ? void 0 : options.formatTime) || ((_configState$configs$10 = configState.configs.format_time) === null || _configState$configs$10 === void 0 ? void 0 : _configState$configs$10.value) || '24';
     var formatTime = {
       inputFormat: (options === null || options === void 0 ? void 0 : options.inputFormat) || ['HH:mm', 'hh:mm A', 'hh:mm'],
       outputFormat: (options === null || options === void 0 ? void 0 : options.outputFormat) || (_formatTime === '24' ? 'HH:mm' : 'hh:mm A'),
@@ -192,28 +174,22 @@ var UtilsProviders = function UtilsProviders(_ref) {
     }
   };
   var parseDistance = function parseDistance(distance) {
-    var _configState$configs$12, _configState$configs$13;
+    var _configState$configs$11, _configState$configs$12;
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     distance = parseFloat(distance) || 0;
     var unit = (options === null || options === void 0 ? void 0 : options.unit) || 'KM';
-    if (((_configState$configs$12 = configState.configs.distance_unit_km) === null || _configState$configs$12 === void 0 ? void 0 : _configState$configs$12.value) === '1') {
+    if (((_configState$configs$11 = configState.configs.distance_unit_km) === null || _configState$configs$11 === void 0 ? void 0 : _configState$configs$11.value) === '1') {
       unit = 'KM';
     }
-    if ((_configState$configs$13 = configState.configs.distance_unit) !== null && _configState$configs$13 !== void 0 && _configState$configs$13.value) {
-      var _configState$configs$14;
-      unit = (_configState$configs$14 = configState.configs.distance_unit) === null || _configState$configs$14 === void 0 ? void 0 : _configState$configs$14.value;
+    if ((_configState$configs$12 = configState.configs.distance_unit) !== null && _configState$configs$12 !== void 0 && _configState$configs$12.value) {
+      var _configState$configs$13;
+      unit = (_configState$configs$13 = configState.configs.distance_unit) === null || _configState$configs$13 === void 0 ? void 0 : _configState$configs$13.value;
     }
     if (unit.toUpperCase() === 'MI') {
-      var dist = distance * 0.621371;
-      if (dist >= 1000) {
-        return "".concat(parseShortenDistance(dist), " ").concat(t('MI', 'mi'));
-      }
+      var dist = distance * 0.621371 / 1000;
       return "".concat(parseNumber(dist, options), " ").concat(t('MI', 'mi'));
     } else {
-      if (distance >= 1000) {
-        return "".concat(parseShortenDistance(distance), " ").concat(t('KM', 'km'));
-      }
-      return "".concat(parseNumber(distance, options), " ").concat(t('KM', 'km'));
+      return "".concat(parseNumber(distance / 1000, options), " ").concat(t('KM', 'km'));
     }
   };
   var getTimeAgo = function getTimeAgo(dateTime, options) {
@@ -242,6 +218,11 @@ var UtilsProviders = function UtilsProviders(_ref) {
   };
   var optimizeImage = function optimizeImage(url, params, fallback) {
     if (!url && fallback) return fallback;
+    params = params && params.length > 0 ? ",".concat(params) : '';
+    if (url != null && url.indexOf('res.cloudinary.com') !== -1) {
+      var parts = url.split('upload');
+      url = "".concat(parts[0], "upload/f_auto,q_auto").concat(params).concat(parts[1]);
+    }
     return url;
   };
   var getOrderState = function getOrderState(num) {
