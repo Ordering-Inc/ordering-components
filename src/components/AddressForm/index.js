70ce187aab6b19f297fc804e86ab97c4ffec5082
Israel Rojas <israelrojas626@gmail.com>
2023-09-05 08:26:38 -0400

    fallback for location

--- src/components/AddressForm/index.js
+++ src/components/AddressForm/index.js
@@ -28,6 +28,7 @@ export const AddressForm = (props) => {
   const userId = props.userId || user?.id
   const accessToken = props.accessToken || token
   const [, { setUserCustomer }] = useCustomer()
+  const defaultCenter = { lat: 40.77473399999999, lng: -73.9653844 }
 
   const [isEdit, setIsEdit] = useState(false)
   const [businessesList, setBusinessesList] = useState({ businesses: [], loading: true, error: null })
@@ -171,7 +172,7 @@ export const AddressForm = (props) => {
       let where = null
       const conditions = []
       const parameters = {
-        location: `${location?.lat},${location?.lng}`,
+        location: location ? `${location?.lat},${location?.lng}` : `${defaultCenter?.lat},${defaultCenter?.lng}`,
         type: options?.type
       }
       if (franchiseId) {
