5b60d7fadec51993d97490337a209da19f86e806
carlosg-ordering <carlos.g@ordering.co>
2023-11-08 14:47:59 -0600

    [FEAT] allow apply offer with offer_id

--- src/contexts/OrderContext/index.js
+++ src/contexts/OrderContext/index.js
@@ -568,14 +568,16 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea, isDisableToa
     }
     try {
       setState({ ...state, loading: true })
+      const _offerData = {
+        user_id: offerData?.userId,
+        business_id: offerData?.business_id,
+        force: offerData?.force ?? false
+      }
+      if (offerData?.coupon) _offerData.coupon = offerData.coupon
+      if (offerData?.offer_id) _offerData.offer_id = offerData.offer_id
       const response = await fetch(`${ordering.root}/carts/add_offer`, {
         method: 'POST',
-        body: JSON.stringify({
-          user_id: offerData.userId,
-          business_id: offerData.business_id,
-          coupon: offerData.coupon,
-          force: offerData.force ?? false
-        }),
+        body: JSON.stringify(_offerData),
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${session.token}`,
