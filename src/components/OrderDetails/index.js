3ab64d78aee4c5176360f19c6f26526877ad3e0c
Carlos Quiroz <77166972+carlosq-ordering@users.noreply.github.com>
2023-11-09 18:57:49 -0400

    [FEAT] driver room for guest (#1161)
    * [FEAT] driver room for guest
    
    * [FEAT] update method to tracking driver in guest mode
    
    ---------
    
    Co-authored-by: Carlos Quiroz <carloseqv29596@gmail.com>

--- src/components/OrderDetails/index.js
+++ src/components/OrderDetails/index.js
@@ -179,7 +179,14 @@ export const OrderDetails = (props) => {
   /**
    * Method to update differents orders status
   */
-  const handleChangeOrderStatus = async (status, isAcceptOrReject = {}) => {
+  const handleChangeOrderStatus = async (status, isAcceptOrReject = {}, options) => {
+    const dataToSave = options?.dataToSave
+    if (dataToSave) {
+      const order = Object.assign(orderState.order, { ...dataToSave, oldStatus: orderState.order?.status })
+      setOrderState({ ...orderState, order })
+      return order
+    }
+
     try {
       const bodyToSend = Object.keys(isAcceptOrReject || {}).length > 0 ? isAcceptOrReject : { status }
       setOrderState({ ...orderState, loading: true })
@@ -451,6 +458,22 @@ export const OrderDetails = (props) => {
     }
   }
 
+  /**
+   * Method to get room socket
+   * @param {String} roomType drivers, orders
+   * @returns socket room
+   */
+  const getRoom = (roomType) => !token
+    ? {
+      room: roomType,
+      project: ordering.project,
+      role: 'public',
+      user_id: hashKey
+    }
+    : roomType === 'orders'
+      ? user?.level === 0 ? 'orders' : `orders_${userCustomerId || user?.id}`
+      : `drivers_${orderState.order?.driver_id}`
+
   useEffect(() => {
     !orderState.loading && loadMessages()
   }, [orderState?.order?.id, orderState?.order?.status, orderState.loading])
@@ -487,7 +510,7 @@ export const OrderDetails = (props) => {
 
   useEffect(() => {
     if (orderState.loading || loading || !socket?.socket) return
-    const handleUpdateOrder = (order) => {
+    const handleUpdateOrderDetails = (order) => {
       if (order?.id !== orderState.order?.id) return
       showToast(ToastType.Info, t('UPDATING_ORDER', 'Updating order...'), 1000)
       delete order.total
@@ -512,35 +535,27 @@ export const OrderDetails = (props) => {
         }
       })
     }
-    const ordersRoom = !token
-      ? {
-        room: 'orders',
-        project: ordering.project,
-        role: 'public',
-        user_id: hashKey
-      } : user?.level === 0
-        ? 'orders'
-        : `orders_${userCustomerId || user?.id}`
 
-    if (!isDisabledOrdersRoom) socket.join(ordersRoom)
+    if (!isDisabledOrdersRoom) socket.join(getRoom('orders'))
     if (orderState.order?.driver_id) {
-      socket.join(`drivers_${orderState.order?.driver_id}`)
+      socket.join(getRoom('drivers'))
     }
     socket.socket.on('connect', () => {
-      if (!isDisabledOrdersRoom) socket.join(ordersRoom)
+      if (!isDisabledOrdersRoom) socket.join(getRoom('orders'))
       if (orderState.order?.driver_id) {
-        socket.join(`drivers_${orderState.order?.driver_id}`)
+        socket.join(getRoom('drivers'))
       }
     })
     socket.on('tracking_driver', handleTrackingDriver)
-    socket.on('update_order', handleUpdateOrder)
+    if (socket?.socket?._callbacks?.$update_order?.find(func => func?.name !== 'handleUpdateOrderDetails')) {
+      socket.on('update_order', handleUpdateOrderDetails)
+    }
     return () => {
-      if (!isDisabledOrdersRoom) socket.leave(ordersRoom)
-      // socket.leave(`drivers_${orderState.order?.driver_id}`)
-      socket.off('update_order', handleUpdateOrder)
+      if (!isDisabledOrdersRoom) socket.leave(getRoom('orders'))
+      socket.off('update_order', handleUpdateOrderDetails)
       socket.off('tracking_driver', handleTrackingDriver)
     }
-  }, [orderState.order, socket?.socket, loading, userCustomerId, orderState.order?.driver_id, orderState.order?.id, hashKey])
+  }, [orderState?.order, socket?.socket, loading, userCustomerId, orderState.order?.driver_id, orderState.order?.id, hashKey])
 
   useEffect(() => {
     if (messages.loading) return
