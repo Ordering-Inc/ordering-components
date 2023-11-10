41b2fb09908726768e2a1fb7ccf2eb48bacd36ba
Jesus David <50583541+YisusJe@users.noreply.github.com>
2023-11-10 07:51:04 -0400

    remove gift cards notifications for business app (#1169)
    * remove gift cards notifications for business app
    
    * remove logs
    
    ---------
    
    Co-authored-by: YisusJe <jesus@ordering.co>

--- src/components/OrderListGroups/index.js
+++ src/components/OrderListGroups/index.js
@@ -861,6 +861,8 @@ export const OrderListGroups = (props) => {
   useEffect(() => {
     if (!socket?.socket || !socket?.socket?.connected) return
     const handleUpdateOrder = (order) => {
+      const isGiftCard = order?.products?.some(product => product?.type === 'gift_card')
+      if (isGiftCard && !isDriverApp) return
       if (session?.user?.level === 2 && businessIDs.length > 0 && !businessIDs.includes(order.business_id)) return
       events.emit('order_updated', order)
       let orderFound = null
@@ -943,6 +945,8 @@ export const OrderListGroups = (props) => {
     }
 
     const handleAddNewOrder = (order) => {
+      const isGiftCard = order?.products?.some(product => product?.type === 'gift_card')
+      if (isGiftCard && !isDriverApp) return
       events.emit('order_added', order)
       handleActionEvent('order_added', order)
       if (notificationStatusses.includes(order?.status)) {
