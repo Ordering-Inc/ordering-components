8e8cb1d5140a550c3fa88e8ad45040416b51aa8f
Carlos Quiroz <carlos.q@ordering.co>
2023-12-18 16:53:24 -0400

    build

--- _modules/components/BusinessInformation/BusinessOption/index.js
+++ _modules/components/BusinessInformation/BusinessOption/index.js
@@ -8,7 +8,7 @@ var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-var BusinessOption = function BusinessOption(props) {
+var BusinessOption = exports.BusinessOption = function BusinessOption(props) {
   var optionToShow = props.optionToShow,
     UIComponent = props.UIComponent;
   var handleClickOption = function handleClickOption(option) {
@@ -35,7 +35,6 @@ var BusinessOption = function BusinessOption(props) {
     openingTime: openingTime
   })));
 };
-exports.BusinessOption = BusinessOption;
 BusinessOption.propTypes = {
   /**
    * UI Component, this must be containt all graphic elements and use parent props
