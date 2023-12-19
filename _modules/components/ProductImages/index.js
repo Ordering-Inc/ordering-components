cd2a151c391299afeb6674c7eae8f42544344baa
YisusJe <jesus@ordering.co>
2023-12-19 18:58:31 -0400

    build

--- _modules/components/ProductImages/index.js
+++ _modules/components/ProductImages/index.js
@@ -8,7 +8,7 @@ var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-var ProductImages = exports.ProductImages = function ProductImages(props) {
+var ProductImages = function ProductImages(props) {
   var hero = props.hero,
     gallery = props.gallery,
     UIComponent = props.UIComponent;
@@ -17,6 +17,7 @@ var ProductImages = exports.ProductImages = function ProductImages(props) {
     gallery: gallery
   })));
 };
+exports.ProductImages = ProductImages;
 ProductImages.propTypes = {
   /**
    * UI Component, this must be containt all graphic elements and use parent props
