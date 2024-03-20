e04fb10ecca42f287e37d5e7482ffbc6e4eb14c4
Carlos Quiroz <carlos.q@ordering.co>
2024-03-20 19:06:42 -0400

    build

--- _modules/components/Emitter/index.js
+++ _modules/components/Emitter/index.js
@@ -8,14 +8,14 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var Emitter = exports.Emitter = /*#__PURE__*/function () {
   function Emitter() {
     _classCallCheck(this, Emitter);
     this._events = {};
   }
-  _createClass(Emitter, [{
+  return _createClass(Emitter, [{
     key: "on",
     value: function on(name, listener) {
       if (!this._events[name]) {
@@ -50,5 +50,4 @@ var Emitter = exports.Emitter = /*#__PURE__*/function () {
       this._events[name].forEach(fireCallbacks);
     }
   }]);
-  return Emitter;
 }();
