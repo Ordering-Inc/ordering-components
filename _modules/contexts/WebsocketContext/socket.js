9ccb499ce8856888c3c85dc1b3b37f42b532361a
Carlos Quiroz <carlos.q@ordering.co>
2024-05-21 22:24:40 -0400

    build

--- _modules/contexts/WebsocketContext/socket.js
+++ _modules/contexts/WebsocketContext/socket.js
@@ -10,7 +10,7 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var Socket = exports.Socket = /*#__PURE__*/function () {
   function Socket(_ref) {
@@ -23,7 +23,7 @@ var Socket = exports.Socket = /*#__PURE__*/function () {
     this.accessToken = accessToken;
     this.queue = [];
   }
-  _createClass(Socket, [{
+  return _createClass(Socket, [{
     key: "connect",
     value: function connect() {
       var _this = this;
@@ -128,5 +128,4 @@ var Socket = exports.Socket = /*#__PURE__*/function () {
       return this;
     }
   }]);
-  return Socket;
 }();
