07eede93ce5c7916a005c81ae6d19bea8e3ca2d8
YisusJe <jesus@ordering.co>
2024-06-10 17:38:27 -0400

    remove method

--- src/contexts/OrderContext/index.js
+++ src/contexts/OrderContext/index.js
@@ -1255,33 +1255,6 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea, isDisableToa
     })
   }
 
-  const handleLogEvent = async (events) => {
-    try {
-      const countryCode = await strategy.getItem('country-code')
-      const headers = {
-        'X-Socket-Id-X': socket?.getId(),
-        'X-Country-Code-X': countryCode
-      }
-      const response = await fetch(`${ordering.root}/tracking_events`, {
-        body: JSON.stringify({
-          events
-        }),
-        headers: {
-          ...headers,
-          Authorization: `Bearer ${session.token}`,
-          'Content-Type': 'application/json'
-        }
-      })
-      const { result, error } = await response.json()
-      if (error) {
-        console.log('Error logging event', result)
-        return
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
   useEffect(() => {
     if (session.loading || languageState.loading) return
     if (session.auth) {
@@ -1403,8 +1376,7 @@ export const OrderProvider = ({ Alert, children, strategy, isAlsea, isDisableToa
     changeCityFilter,
     confirmMultiCarts,
     addMultiProduct,
-    handleOrderStateLoading,
-    handleLogEvent
+    handleOrderStateLoading
   }
 
   const copyState = JSON.parse(JSON.stringify(state))
