57d4984accac8acebc4b1d252dc2838ea44a2258
Jesus David <50583541+YisusJe@users.noreply.github.com>
2024-06-25 09:53:34 -0400

    [FIX] search duplicated products (#1263)
    Co-authored-by: YisusJe <jesus@ordering.co>

--- src/components/BusinessAndProductList/index.js
+++ src/components/BusinessAndProductList/index.js
@@ -516,7 +516,9 @@ export const BusinessAndProductList = (props) => {
           loading: false,
           products: categorySelected.id === 'featured'
             ? productsListFeatured
-            : searchValue ? [...productsListFeatured, ...productsList] : [...productsListFeatured, ...curCategoryState.products.concat(productsList)]
+            : searchValue
+              ? [...productsListFeatured, ...productsList].filter((product, i, _hash) => _hash.findIndex(_product => _product?.id === product?.id) === i)
+              : [...productsListFeatured, ...curCategoryState.products.concat(productsList)]
         }
 
         categoriesState[categoryKey] = newcategoryState
