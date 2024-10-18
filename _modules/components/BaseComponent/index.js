50a4f11ae671ada4d3b2842ba33fce548565b997
Carlos Quiroz <carlos.q@ordering.co>
2024-10-18 01:49:36 -0400

    build

--- _modules/components/BaseComponent/index.js
+++ _modules/components/BaseComponent/index.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 exports.BaseComponent = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 var BaseComponent = exports.BaseComponent = function BaseComponent(props) {
   var UIComponent = props.UIComponent;
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, UIComponent && /*#__PURE__*/_react.default.createElement(UIComponent, props));
